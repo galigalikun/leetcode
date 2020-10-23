e2dbc5504996d0d7d853815b45018c6160bba923
akaishi <galigalikun@gmail.com>
2020-10-23 19:11:34 +0900

    使わないところ削除

--- src/main.rs
+++ src/main.rs
@@ -56,29 +56,6 @@ impl ListNode {
 }
 
 impl Solution {
-    fn number(l: Option<Box<ListNode>>) -> i64 {
-        let mut n:i64 = 0;
-        let mut list = l;
-        let mut i = 0;
-        loop {
-            match list {
-
-                Some(data) => {
-                    n += i64::pow(10, i) * (data.val as i64);
-                    list = data.next;
-                    i+=1;
-                }
-                _ => {
-                    break;
-                }
-            }
-        }
-
-        return n;
-    }
-    fn digit(n: i64) -> u32 {
-        return n.to_string().len() as u32;
-    }
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut a: Option<Box<ListNode>> = None;
         let mut list1 = l1;
