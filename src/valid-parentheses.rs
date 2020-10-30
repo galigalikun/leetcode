1c169b90148cbc19b3cde808a1cf91e032c7d5b3
akaishi <galigalikun@gmail.com>
2020-10-31 07:30:04 +0900

    add valid-parentheses

---
+++ src/valid-parentheses.rs
@@ -0,0 +1,64 @@
+fn main() {
+    assert_eq!(Solution::is_valid("()".to_string()), true);
+    assert_eq!(Solution::is_valid("()[]{}".to_string()), true);
+    assert_eq!(Solution::is_valid("(]".to_string()), false);
+    assert_eq!(Solution::is_valid("([)]".to_string()), false);
+    assert_eq!(Solution::is_valid("{[]}".to_string()), true);
+    assert_eq!(Solution::is_valid("[".to_string()), false);
+    assert_eq!(Solution::is_valid("((".to_string()), false);
+    assert_eq!(Solution::is_valid("))".to_string()), false);
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        if s.len() % 2 == 1 {
+            return false;
+        }
+        let mut prev = Vec::new();
+        for c in s.as_str().chars() {
+            match c {
+                '(' => {
+                    prev.push(c);
+                }
+                ')' => {
+                    if let Some(p) = prev.pop() {
+                        if p != '(' {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                '{' => {
+                    prev.push(c);
+                }
+                '}' => {
+                    if let Some(p) = prev.pop() {
+                        if p != '{' {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                '[' => {
+                    prev.push(c);
+                }
+                ']' => {
+                    if let Some(p) = prev.pop() {
+                        if p != '[' {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        return prev.len() == 0;
+    }
+}
