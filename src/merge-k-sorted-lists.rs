1ac9a7954ce6bbf8b130dcf052a936ceda76e3b5
akaishi <galigalikun@gmail.com>
2020-11-01 07:42:18 +0900

    add merge-k-sorted-lists

---
+++ src/merge-k-sorted-lists.rs
@@ -0,0 +1,97 @@
+fn main() {
+    assert_eq!(Solution::merge_k_lists(vec![Some(Box::new(ListNode{
+        val:1,
+        next:Some(Box::new(ListNode{
+            val:4,
+            next:Some(Box::new(ListNode{
+                val:5,
+                next:None
+            }))
+        }))
+    })),Some(Box::new(ListNode{
+        val:1,
+        next:Some(Box::new(ListNode{
+            val:3,
+            next:Some(Box::new(ListNode{
+                val:4,
+                next:None
+            }))
+        }))
+    })),Some(Box::new(ListNode{
+        val:2,
+        next:Some(Box::new(ListNode{
+            val:6,
+            next:None
+        }))
+    }))]), Some(Box::new(ListNode{
+        val:1,
+        next:Some(Box::new(ListNode{
+            val:1,
+            next:Some(Box::new(ListNode{
+                val:2,
+                next:Some(Box::new(ListNode{
+                    val:3,
+                    next:Some(Box::new(ListNode{
+                        val:4,
+                        next:Some(Box::new(ListNode{
+                            val:4,
+                            next:Some(Box::new(ListNode{
+                                val:5,
+                                next:Some(Box::new(ListNode{
+                                    val:6,
+                                    next:None
+                                }))
+                            }))
+                        }))
+                    }))
+                }))
+            }))
+        }))
+    })));
+    assert_eq!(Solution::merge_k_lists(vec![]), None);
+}
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+pub struct Solution{}
+impl Solution {
+    pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+        let mut buf = Vec::new();
+
+        for mut l in lists {
+            loop {
+                if let Some(n) = l {
+                    buf.push(n.val);
+                    l = n.next;
+                } else {
+                    break;
+                }
+            }
+        }
+        let mut result:Option<Box<ListNode>> = None;
+        buf.sort_by(|x, y| y.cmp(x));
+
+        for b in buf {
+            result = Some(Box::new(ListNode{
+                val: b,
+                next:result
+            }));
+        }
+
+        return result;
+    }
+}
