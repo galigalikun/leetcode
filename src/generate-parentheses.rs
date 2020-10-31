6da3bc16a6f42b3ba6389583b23024340ac9b57f
akaishi <galigalikun@gmail.com>
2020-11-01 07:42:03 +0900

    add generate-parentheses

---
+++ src/generate-parentheses.rs
@@ -0,0 +1,21 @@
+fn main() {
+    assert_eq!(Solution::generate_parenthesis(3), vec!["((()))","(()())","(())()","()(())","()()()"]);
+    assert_eq!(Solution::generate_parenthesis(1), vec!["()"]);
+}
+
+pub struct Solution{}
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut result = Vec::new();
+        let mut s = "".to_string();
+        for _ in 0..n {
+            s.push_str("()");
+        }
+        /*
+        ss.push(n).push_str("(").push(n).push_str(")");
+        ss.push(n).push_str("((").push(n).push_str(")");
+        */
+        result.push(s);
+        return result;
+    }
+}
