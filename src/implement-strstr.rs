4ad532a97456d58c376df948c6ffefa0cae03a77
akaishi <galigalikun@gmail.com>
2020-11-03 07:15:31 +0900

    add implement-strstr

---
+++ src/implement-strstr.rs
@@ -0,0 +1,18 @@
+fn main() {
+    assert_eq!(Solution::str_str("hello".to_string(), "ll".to_string()), 2);
+    assert_eq!(
+        Solution::str_str("aaaaa".to_string(), "bba".to_string()),
+        -1
+    );
+    assert_eq!(Solution::str_str("".to_string(), "".to_string()), 0);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        if let Some(p) = haystack.find(needle.as_str()) {
+            return p as i32;
+        }
+        return -1;
+    }
+}
