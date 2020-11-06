e9fc7671d9d1a6f37b3887f0ecf642a94a0b05ba
akaishi <galigalikun@gmail.com>
2020-11-07 08:45:09 +0900

    add combination-sum

--- src/combination-sum.rs
+++ src/combination-sum.rs
@@ -3,34 +3,52 @@ fn main() {
         Solution::combination_sum(vec![2, 3, 6, 7], 7),
         vec![vec![2, 2, 3], vec![7]]
     );
+    assert_eq!(
+        Solution::combination_sum(vec![2, 3, 5], 8),
+        vec![vec![2, 2, 2, 2], vec![2, 3, 3], vec![3, 5]]
+    );
 }
 
 pub struct Solution {}
 impl Solution {
-    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    fn re_combination_sum(
+        candidates: &mut Vec<i32>,
+        target: i32,
+        index: Option<usize>,
+    ) -> Vec<Vec<i32>> {
         let len = candidates.len();
-        let mut ret = Vec::new();
+        let mut result = Vec::new();
         for i in 0..len {
+            if Some(i) == index {
+                continue;
+            }
             let num = candidates[i];
-            if target > num {
-                if target / num > 1 {
+            if target >= num {
+                if target % num == 0 {
+                    let mut v = Vec::new();
+                    for _ in 0..target / num {
+                        v.push(num);
+                    }
+                    result.push(v);
+                } else {
                     for j in 1..=target / num {
-                        let mut aaa = Vec::new();
-                        aaa.push(num);
-                        for k in i + 1..len {
-                            if (target - num * j - candidates[k]) == 0 {
-                                let mut bbb = aaa;
-                                bbb.push(candidates[k]);
-                                ret.push(bbb);
-                                break;
+                        for r in Solution::re_combination_sum(candidates, target - num * j, Some(i))
+                        {
+                            let mut v = Vec::new();
+                            for _ in 0..j {
+                                v.push(num);
                             }
+                            v.extend(r);
+                            result.push(v);
                         }
                     }
                 }
-            } else if target == num {
-                ret.push(vec![num]);
             }
         }
-        return ret;
+        return result;
+    }
+    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut zzz = candidates;
+        return Solution::re_combination_sum(&mut zzz, target, None);
     }
 }
