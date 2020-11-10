cebb4e0b51c991eb6b6dd7dc74ba8ba5ac6b4f53
akaishi <galigalikun@gmail.com>
2020-11-10 17:54:33 +0900

    add length-of-last-word

---
+++ src/length-of-last-word.rs
@@ -0,0 +1,15 @@
+fn main() {
+    assert_eq!(Solution::length_of_last_word("Hello World".to_string()), 5);
+    assert_eq!(Solution::length_of_last_word("Today is a nice day".to_string()), 3);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        if let Some(&c) = s.split_whitespace().collect::<Vec<&str>>().last() {
+            return c.len() as i32;
+        }
+
+        return 0;
+    }
+}
