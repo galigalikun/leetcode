89d0d59ee79b3ca5b08033134441699c5f778ce9
akaishi <galigalikun@gmail.com>
2020-11-13 06:47:21 +0900

    add sqrtx

---
+++ src/sqrtx.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::my_sqrt(4), 2);
+    assert_eq!(Solution::my_sqrt(8), 2);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn my_sqrt(x: i32) -> i32 {
+        return (x as f64).sqrt() as i32;
+    }
+}
