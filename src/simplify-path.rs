73c73de4f036460c63247d71f824f99fb7f0179c
akaishi <galigalikun@gmail.com>
2020-11-14 08:56:55 +0900

    add simplify-path

---
+++ src/simplify-path.rs
@@ -0,0 +1,38 @@
+fn main() {
+    assert_eq!(
+        Solution::simplify_path("/home/".to_string()),
+        "/home".to_string()
+    );
+    assert_eq!(
+        Solution::simplify_path("/../".to_string()),
+        "/".to_string()
+    );
+    assert_eq!(
+        Solution::simplify_path("/a/./b/../../c/".to_string()),
+        "/c".to_string()
+    );
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn simplify_path(path: String) -> String {
+        let mut pieces = path.split("/");
+        let mut node = Vec::new();
+        while let Some(p) = pieces.next() {
+            if p.len() > 0 {
+                if p == "." {
+                } else if p == ".." {
+                    if node.len() > 0 {
+                        node.pop();
+                    }
+                } else {
+                    node.push("/".to_owned() + p);
+                }
+            }
+        }
+        if node.len() == 0 {
+            return "/".to_string();
+        }
+        return node.join("");
+    }
+}
