d81b9615333669c7f94fb99d4cbb36c8c5bb5810
akaishi <galigalikun@gmail.com>
2020-11-24 08:45:57 +0900

    add remove-duplicates-from-sorted-list-ii

---
+++ src/remove-duplicates-from-sorted-list-ii.rs
@@ -0,0 +1,96 @@
+fn main() {
+    assert_eq!(
+        Solution::delete_duplicates(Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 3,
+                        next: Some(Box::new(ListNode {
+                            val: 4,
+                            next: Some(Box::new(ListNode {
+                                val: 4,
+                                next: Some(Box::new(ListNode { val: 5, next: None }))
+                            }))
+                        }))
+                    }))
+                }))
+            }))
+        }))),
+        Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode { val: 5, next: None }))
+            }))
+        }))
+    );
+    assert_eq!(
+        Solution::delete_duplicates(Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 1,
+                    next: Some(Box::new(ListNode {
+                        val: 2,
+                        next: Some(Box::new(ListNode { val: 3, next: None }))
+                    }))
+                }))
+            }))
+        }))),
+        Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 3, next: None }))
+        }))
+    );
+}
+
+pub struct Solution {}
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+impl Solution {
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut list = head;
+        let mut prev: Option<i32> = None;
+        let mut work = Vec::new();
+        loop {
+            if let Some(h) = list {
+                if prev == Some(h.val) {
+                    work.retain(|&x| x != h.val);
+                } else {
+                    work.push(h.val);
+                }
+
+                prev = Some(h.val);
+                list = h.next;
+            } else {
+                break;
+            }
+        }
+
+        let mut result: Option<Box<ListNode>> = None;
+        for w in work.into_iter().rev() {
+            result = Some(Box::new(ListNode {
+                val: w,
+                next: result,
+            }));
+        }
+
+        return result;
+    }
+}
