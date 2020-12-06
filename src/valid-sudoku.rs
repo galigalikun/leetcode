c1ce312224e7d210cac1bff25788aa75a0af1328
akaishi <galigalikun@gmail.com>
2020-12-06 09:47:16 +0900

    add valid-sudoku

--- src/valid-sudoku.rs
+++ src/valid-sudoku.rs
@@ -13,26 +13,66 @@ fn main() {
         ]),
         true
     );
+    assert_eq!(
+        Solution::is_valid_sudoku(vec![
+            vec!['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+            vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            vec!['.', '.', '.', '.', '8', '.', '.', '7', '9']
+        ]),
+        false
+    );
 }
 
 pub struct Solution {}
 impl Solution {
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        let mut bb = Vec::new();
-        bb.push(Vec::new());
-        bb[0].push(5);
-        bb[0].push(3);
-        bb[0].push(1|2|4); // not 5,3,6,9,8,7
-        bb[0].push(2|4|6|8); // not 5,3,7,1,9,5
-        for x in 0..=9 {
-
-            let mut num =
-            for y in 0..=9 {
-
-                let n = board[x][y] as i32 - 48;
-                if n >0 && 10 > n {
-
+        let mut cell: Vec<Vec<char>> = vec![
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+        ];
+        let mut vertical: Vec<Vec<char>> = vec![
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+        ];
+        for row in 0..board.len() {
+            let mut line: Vec<char> = Vec::with_capacity(board[row].len());
+            for col in 0..board[row].len() {
+                if board[row][col] == '.' {
+                    continue;
                 }
+                let key = 3 * (row / 3) + col / 3;
+                if None != line.iter().find(|&x| x == &board[row][col]) {
+                    return false;
+                }
+                if None != cell[key].iter().find(|&x| x == &board[row][col]) {
+                    return false;
+                }
+                if None != vertical[col].iter().find(|&x| x == &board[row][col]) {
+                    return false;
+                }
+                line.push(board[row][col]);
+                cell[key].push(board[row][col]);
+                vertical[col].push(board[row][col]);
             }
         }
         return true;
