0dee05e273191579b43c6c01c6dbe5db764fb7c3
akaishi <galigalikun@gmail.com>
2020-12-07 22:18:13 +0900

    add single-number

---
+++ src/single-number.rs
@@ -0,0 +1,27 @@
+fn main() {
+    assert_eq!(Solution::single_number(vec![2, 2, 1]), 1);
+    assert_eq!(Solution::single_number(vec![4, 1, 2, 1, 2]), 4);
+    assert_eq!(Solution::single_number(vec![1]), 1);
+}
+
+pub struct Solution {}
+use std::collections::HashMap;
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        let mut map = HashMap::new();
+        for n in nums {
+            if let Some(x) = map.get_mut(&n) {
+                *x += 1;
+            } else {
+                map.insert(n, 1);
+            }
+        }
+        for (k, v) in map {
+            if v == 1 {
+                return k;
+            }
+        }
+
+        return 0;
+    }
+}
