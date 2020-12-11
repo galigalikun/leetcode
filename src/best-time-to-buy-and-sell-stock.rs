e506334c949d41715f14c8bfa3c0bd5fad1c533e
akaishi <galigalikun@gmail.com>
2020-12-12 07:33:12 +0900

    add best-time-to-buy-and-sell-stock

---
+++ src/best-time-to-buy-and-sell-stock.rs
@@ -0,0 +1,24 @@
+fn main() {
+    assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
+    assert_eq!(Solution::max_profit(vec![7, 6, 4, 3, 1]), 0);
+    assert_eq!(Solution::max_profit(vec![]), 0);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut max = 0;
+        if prices.len() > 0 {
+            for i in 0..prices.len() - 1 {
+                for j in i + 1..prices.len() {
+                    let diff = prices[j] - prices[i];
+                    if diff > max {
+                        max = diff;
+                    }
+                }
+            }
+        }
+
+        return max;
+    }
+}
