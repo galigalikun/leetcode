d1ccc2cd9bec28de0a746bd8b8702519f1f81c4a
akaishi <galigalikun@gmail.com>
2020-12-14 19:24:40 +0900

    wip generate-parentheses

--- src/generate-parentheses.rs
+++ src/generate-parentheses.rs
@@ -3,15 +3,15 @@ fn main() {
         Solution::generate_parenthesis(3),
         vec!["((()))", "(()())", "(())()", "()(())", "()()()"]
     );
-    assert_eq!(Solution::generate_parenthesis(1), vec!["()"]);
-    assert_eq!(Solution::generate_parenthesis(2), vec!["(())", "()()"]);
-    assert_eq!(
-        Solution::generate_parenthesis(4),
-        vec![
-            "(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()",
-            "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"
-        ]
-    );
+    // assert_eq!(Solution::generate_parenthesis(1), vec!["()"]);
+    // assert_eq!(Solution::generate_parenthesis(2), vec!["(())", "()()"]);
+    // assert_eq!(
+    //     Solution::generate_parenthesis(4),
+    //     vec![
+    //         "(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()",
+    //         "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"
+    //     ]
+    // );
     // assert_eq!(
     //     Solution::generate_parenthesis(5),
     //     vec![
@@ -62,10 +62,28 @@ fn main() {
 }
 
 pub struct Solution {}
+// https://medium.com/@chyanpin/solving-leetcodes-challenge-22-generate-parentheses-2ba613397c06
 impl Solution {
+    fn breath_first_search(result: &mut Vec<String>, s: String, n: i32, left: i32, right: i32) {
+        if left == n && right == n {
+            result.push(s);
+            return;
+        }
+        if left != n {
+            let mut ss = s.clone();
+            ss.push('(');
+            Solution::breath_first_search(result, ss, n, left + 1, right);
+        }
+        if right != n {
+            let mut ss = s.clone();
+            ss.push(')');
+            Solution::breath_first_search(result, ss, n, left, right + 1);
+        }
+    }
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
         let mut result = Vec::new();
-
+        // let mut hello = String::from("Hello, ");
+        Solution::breath_first_search(&mut result, "".to_string(), n, 0, 0);
         /*
         1 -> 1
         2 -> 2
@@ -73,7 +91,7 @@ impl Solution {
         4 -> 14
         5 -> 42
         ((((
-        */
+
         let base = ['('].repeat(n as usize);
         {
             let mut b = base.clone();
@@ -85,8 +103,8 @@ impl Solution {
             for i in 0..(n - 2) {
                 for j in 0..(n - 1) {
                     let mut b = base.clone();
-                    for k in 0..=j-i {
-                        b.insert((n - (i+1) + k) as usize, ')');
+                    for k in 0..=j - i {
+                        b.insert((n - (i + 1) + k) as usize, ')');
                     }
                     for _k in 1..n - j + i {
                         b.push(')');
@@ -114,6 +132,7 @@ impl Solution {
 
             result.push(b.iter().collect::<String>());
         }
+        */
         return result;
     }
 }
