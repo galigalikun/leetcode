d6453104eddca076bf989b85fd891edf7089f572
akaishi <galigalikun@gmail.com>
2020-12-20 21:38:46 +0900

    add insertion-sort-list

---
+++ src/insertion-sort-list.rs
@@ -0,0 +1,64 @@
+fn main() {
+    assert_eq!(
+        Solution::insertion_sort_list(Some(Box::new(ListNode {
+            val: 4,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 1,
+                    next: Some(Box::new(ListNode { val: 3, next: None }))
+                }))
+            }))
+        }))),
+        Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode { val: 4, next: None }))
+                }))
+            }))
+        }))
+    );
+}
+
+pub struct Solution {}
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+impl Solution {
+    pub fn insertion_sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut list = head;
+        let mut work = Vec::new();
+        loop {
+            if let Some(l) = list {
+                work.push(l.val);
+                list = l.next;
+            } else {
+                break;
+            }
+        }
+        work.sort_by(|a, b| b.cmp(a));
+
+        let mut result: Option<Box<ListNode>> = None;
+        for w in work {
+            result = Some(Box::new(ListNode {
+                val: w,
+                next: result,
+            }));
+        }
+
+        return result;
+    }
+}
