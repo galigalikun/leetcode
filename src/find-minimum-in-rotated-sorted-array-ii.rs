c608161eb5503ee6ecee011bef0ce0d3d356f016
akaishi <galigalikun@gmail.com>
2020-12-27 09:11:27 +0900

    add find-minimum-in-rotated-sorted-array-ii

---
+++ src/find-minimum-in-rotated-sorted-array-ii.rs
@@ -0,0 +1,14 @@
+fn main() {
+    assert_eq!(Solution::find_min(vec![1, 3, 5]), 1);
+    assert_eq!(Solution::find_min(vec![2,2,2,0,1]), 0);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        if let Some(m) = nums.into_iter().min() {
+            return m;
+        }
+        return 0;
+    }
+}
