18965200eff2af38c7c7f9963b6614f9b9175ec7
akaishi <galigalikun@gmail.com>
2020-12-29 14:50:30 +0900

    add minimum-path-sum

--- src/minimum-path-sum.rs
+++ src/minimum-path-sum.rs
@@ -17,28 +17,47 @@ pub fn main() {
 pub struct Solution {}
 // https://www.programcreek.com/2014/05/leetcode-minimum-path-sum-java/
 impl Solution {
-    fn dfs(i: usize, j: usize, grid: Vec<Vec<i32>>) -> i32 {
-        if i == grid.len() - 1 && j == grid[0].len() - 1 {
-            return grid[i][j];
-        }
-
-        if i < grid.len() - 1 && j < grid[0].len() - 1 {
-            let r1 = grid[i][j] + Solution::dfs(i + 1, j, grid.clone());
-            let r2 = grid[i][j] + Solution::dfs(i, j + 1, grid);
-            return std::cmp::min(r1, r2);
-        }
-
-        if i < grid.len() - 1 {
-            return grid[i][j] + Solution::dfs(i + 1, j, grid);
-        }
-
-        return grid[i][j] + Solution::dfs(i, j + 1, grid);
-    }
     pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
         /*
         1,2,5
         3,2,1
         */
-        return Solution::dfs(0, 0, grid);
+        if grid.len() == 0 {
+            return 0;
+        }
+
+        let m = grid.len();
+        let n = grid[0].len();
+
+        let mut dp = Vec::with_capacity(m);
+
+        for i in 0..m {
+            dp.push(vec![0]);
+            for _j in 1..n {
+                dp[i].push(0);
+            }
+        }
+
+        dp[0][0] = grid[0][0];
+
+        for i in 1..n {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        for j in 1..m {
+            dp[j][0] = dp[j - 1][0] + grid[j][0];
+        }
+
+        for i in 1..m {
+            for j in 1..n {
+                if dp[i - 1][j] > dp[i][j - 1] {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
