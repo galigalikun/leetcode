6b5e91ec93dba9c5a943c16f641871bed0992a87
akaishi <galigalikun@gmail.com>
2021-01-14 20:49:47 +0900

    add find-peak-element

---
+++ src/find-peak-element.rs
@@ -0,0 +1,19 @@
+fn main() {
+    assert_eq!(Solution::find_peak_element(vec![1, 2, 3, 1]), 2);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        let mut max = std::i32::MIN;
+        let mut idx:i32 = 0;
+        for i in 0..nums.len() {
+            if nums[i] > max {
+                max = nums[i];
+                idx = i as i32;
+            }
+        }
+
+        return idx;
+    }
+}
