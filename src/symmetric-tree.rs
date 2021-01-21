b4f82907c1416e0d7460e5ff4324dcc0d6f51ff6
akaishi <galigalikun@gmail.com>
2021-01-21 17:13:19 +0900

    add symmetric-tre

---
+++ src/symmetric-tree.rs
@@ -0,0 +1,103 @@
+fn main() {
+    assert_eq!(
+        Solution::is_symmetric(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                })))
+            })))
+        })))),
+        true
+    );
+
+    assert_eq!(
+        Solution::is_symmetric(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                })))
+            })))
+        })))),
+        false
+    );
+
+    assert_eq!(Solution::is_symmetric(None), true);
+}
+
+pub struct Solution {}
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+// https://github.com/halfrost/rust-learning/blob/1a127ef960383a9a5e4af94c628182789f192133/solution/502.rs
+impl Solution {
+    fn both(left: Option<Rc<RefCell<TreeNode>>>, right: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match (left, right) {
+            (Some(l), Some(r)) => {
+                return l.borrow().val == r.borrow().val
+                    && Solution::both(l.borrow().left.clone(), r.borrow().right.clone())
+                    && Solution::both(r.borrow().left.clone(), l.borrow().right.clone())
+            }
+            (None, None) => true,
+            _ => false,
+        }
+    }
+    pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        if let Some(n) = root {
+            return Solution::both(n.borrow().left.clone(), n.borrow().right.clone());
+        }
+        return true;
+    }
+}
