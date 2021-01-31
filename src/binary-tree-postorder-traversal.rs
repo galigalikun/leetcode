81d579f09eba8e484d7dc06901f3c48752f87e0e
akaishi <galigalikun@gmail.com>
2021-01-31 10:14:43 +0900

    add binary-tree-postorder-traversal

---
+++ src/binary-tree-postorder-traversal.rs
@@ -0,0 +1,117 @@
+fn main() {
+    assert_eq!(
+        Solution::postorder_traversal(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                }))),
+                right: None
+            })))
+        })))),
+        vec![3, 2, 1]
+    );
+
+    assert_eq!(Solution::postorder_traversal(None), vec![]);
+    assert_eq!(
+        Solution::postorder_traversal(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: None
+        })))),
+        vec![1]
+    );
+
+    assert_eq!(
+        Solution::postorder_traversal(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None
+            }))),
+            right: None
+        })))),
+        vec![2, 1]
+    );
+
+    assert_eq!(
+        Solution::postorder_traversal(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None
+            })))
+        })))),
+        vec![2, 1]
+    );
+
+    assert_eq!(
+        Solution::postorder_traversal(Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: None
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None
+            })))
+        })))),
+        vec![1, 2, 3]
+    );
+}
+
+pub struct Solution {}
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    fn helper(
+        result: &mut Vec<i32>,
+        left: Option<Rc<RefCell<TreeNode>>>,
+        right: Option<Rc<RefCell<TreeNode>>>,
+    ) {
+        if let Some(r) = right {
+            result.push(r.borrow().val);
+            Solution::helper(result, r.borrow().left.clone(), r.borrow().right.clone());
+        }
+        if let Some(l) = left {
+            result.push(l.borrow().val);
+            Solution::helper(result, l.borrow().left.clone(), l.borrow().right.clone());
+        }
+    }
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let result = &mut vec![];
+        if let Some(n) = root {
+            result.push(n.borrow().val);
+            Solution::helper(result, n.borrow().left.clone(), n.borrow().right.clone());
+        }
+        return result.iter().rev().map(|&x| x).collect::<Vec<_>>();
+    }
+}
