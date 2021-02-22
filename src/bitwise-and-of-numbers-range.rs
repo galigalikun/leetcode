ea0dc8aa936053af3877731fb1089f7eb8916154
akaishi <galigalikun@gmail.com>
2021-02-23 08:55:00 +0900

    add bitwise-and-of-numbers-range

---
+++ src/bitwise-and-of-numbers-range.rs
@@ -0,0 +1,24 @@
+fn main() {
+    assert_eq!(Solution::range_bitwise_and(5, 7), 4);
+
+    assert_eq!(Solution::range_bitwise_and(0, 1), 0);
+
+    assert_eq!(Solution::range_bitwise_and(4, 8), 0);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn range_bitwise_and(m: i32, n: i32) -> i32 {
+        let mut result: Option<i32> = None;
+        for n in m..=n {
+            match result.as_mut() {
+                Some(v) => *v &= n,
+                None => result = Some(n),
+            }
+        }
+        if let Some(r) = result {
+            return r;
+        }
+        return 0;
+    }
+}
