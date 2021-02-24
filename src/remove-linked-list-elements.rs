a670cd8a5ab4b32fff3e43d4700e918d29d57edb
akaishi <galigalikun@gmail.com>
2021-02-24 09:02:48 +0900

    add remove-linked-list-elements

---
+++ src/remove-linked-list-elements.rs
@@ -0,0 +1,77 @@
+fn main() {
+    assert_eq!(
+        Solution::remove_elements(
+            Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode {
+                        val: 6,
+                        next: Some(Box::new(ListNode {
+                            val: 3,
+                            next: Some(Box::new(ListNode {
+                                val: 4,
+                                next: Some(Box::new(ListNode {
+                                    val: 5,
+                                    next: Some(Box::new(ListNode { val: 6, next: None }))
+                                }))
+                            }))
+                        }))
+                    }))
+                }))
+            })),
+            6
+        ),
+        Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode { val: 5, next: None }))
+                    }))
+                }))
+            }))
+        }))
+    );
+}
+
+pub struct Solution {}
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+impl Solution {
+    fn helper(result: &mut Vec<i32>, head: Option<Box<ListNode>>, val: i32) {
+        if let Some(h) = head {
+            if h.val != val {
+                result.push(h.val);
+            }
+            Solution::helper(result, h.next, val);
+        }
+    }
+    pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        let result = &mut vec![];
+        Solution::helper(result, head, val);
+
+        let mut out: Option<Box<ListNode>> = None;
+        for v in result.iter().rev() {
+            out = Some(Box::new(ListNode{
+                val:*v,
+                next:out
+            }));
+        }
+        return out;
+    }
+}
