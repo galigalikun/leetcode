783a10a919f8e762802b8319b5e7792e37040246
akaishi <galigalikun@gmail.com>
2021-03-04 20:52:43 +0900

    add kth-largest-element-in-an-array

---
+++ src/kth-largest-element-in-an-array.rs
@@ -0,0 +1,16 @@
+fn main() {
+    assert_eq!(Solution::find_kth_largest(vec![3, 2, 1, 5, 6, 4], 2), 5);
+    assert_eq!(
+        Solution::find_kth_largest(vec![3, 2, 3, 1, 2, 4, 5, 5, 6], 4),
+        4
+    )
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+        let mut work = nums;
+        work.sort();
+        return work[work.len() - k as usize];
+    }
+}
