43129a4782c2fd065231110083172441509e905a
akaishi <galigalikun@gmail.com>
2021-03-08 21:32:46 +0900

    add count-complete-tree-nodes

---
+++ src/count-complete-tree-nodes.rs
@@ -0,0 +1,75 @@
+fn main() {
+    assert_eq!(
+        Solution::count_nodes(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: None
+                }))),
+                right: None
+            })))
+        })))),
+        6
+    );
+    assert_eq!(Solution::count_nodes(None), 0);
+    assert_eq!(
+        Solution::count_nodes(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: None
+        })))),
+        1
+    );
+}
+
+pub struct Solution {}
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    fn helper(cnt: &mut i32, root: Option<Rc<RefCell<TreeNode>>>) {
+        if let Some(r) = root {
+            *cnt += 1;
+            Solution::helper(cnt, r.borrow().left.clone());
+            Solution::helper(cnt, r.borrow().right.clone());
+        }
+    }
+    pub fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut cnt = 0;
+        Solution::helper(&mut cnt, root);
+        return cnt;
+    }
+}
