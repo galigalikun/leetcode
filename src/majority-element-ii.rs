54bde3cd7d6f93fc573f90c83fb9870f9a17d700
akaishi <galigalikun@gmail.com>
2021-03-13 19:51:33 +0900

    add majority-element-ii

---
+++ src/majority-element-ii.rs
@@ -0,0 +1,29 @@
+fn main() {
+    assert_eq!(Solution::majority_element(vec![3, 2, 3]), vec![3]);
+    assert_eq!(Solution::majority_element(vec![1]), vec![1]);
+    assert_eq!(Solution::majority_element(vec![1, 2]), vec![1, 2]);
+}
+
+pub struct Solution {}
+use std::collections::HashMap;
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len() / 3;
+        let mut map = HashMap::new();
+        for n in nums {
+            if let Some(x) = map.get_mut(&n) {
+                *x += 1;
+            } else {
+                map.insert(n, 1);
+            }
+        }
+        let mut result = vec![];
+        for (k, v) in map {
+            if v > n {
+                result.push(k);
+            }
+        }
+
+        return result;
+    }
+}
