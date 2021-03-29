1ec38f03689342e74040510c4a831629d110c4b0
akaishi <galigalikun@gmail.com>
2021-03-30 07:28:53 +0900

    add longest-valid-parentheses

--- src/longest-valid-parentheses.rs
+++ src/longest-valid-parentheses.rs
@@ -1,9 +1,10 @@
 fn main() {
-    // assert_eq!(Solution::longest_valid_parentheses("(()".to_string()), 2);
-    // assert_eq!(Solution::longest_valid_parentheses(")()())".to_string()), 4);
-    // assert_eq!(Solution::longest_valid_parentheses("".to_string()), 0);
-    // assert_eq!(Solution::longest_valid_parentheses("()(()".to_string()), 2);
-    // assert_eq!(Solution::longest_valid_parentheses(")()())()()(".to_string()),4);
+    assert_eq!(Solution::longest_valid_parentheses("()".to_string()), 2);
+    assert_eq!(Solution::longest_valid_parentheses("(()".to_string()), 2);
+    assert_eq!(Solution::longest_valid_parentheses(")()())".to_string()), 4);
+    assert_eq!(Solution::longest_valid_parentheses("".to_string()), 0);
+    assert_eq!(Solution::longest_valid_parentheses("()(()".to_string()), 2);
+    assert_eq!(Solution::longest_valid_parentheses(")()())()()(".to_string()),4);
     assert_eq!(
         Solution::longest_valid_parentheses("(()(((()".to_string()),
         2
@@ -11,46 +12,33 @@ fn main() {
 }
 
 pub struct Solution {}
+// https://programmerstart.com/article/1255166812/
 impl Solution {
     pub fn longest_valid_parentheses(s: String) -> i32 {
-        let mut a = Vec::new();
-        let mut b = 0;
+        let mut a:Vec<i32> = vec![-1];
         let mut n = 0;
         let mut ret: i32 = 0;
         for c in s.as_str().chars() {
-            match c {
-                '(' => {
-                    a.push(c);
-                    n += 1;
-                }
-                ')' => {
-                    if a.len() > 0 {
+            if c == '(' {
+                a.push(n);
+            } else {
+                if let Some(&l) = a.last() {
+                    if Some('(') == s.as_str().chars().nth(l as usize) {
                         a.pop();
-                        n += 1;
-                        if a.len() == 0 {
-                            b = n + b;
-                            if b > ret {
-                                ret = b;
+                        if let Some(&l) = a.last() {
+                            let new_len = n as i32 - l;
+                            if new_len > ret {
+                                ret = new_len;
                             }
-                            n = 0;
                         }
                     } else {
-                        if n > ret {
-                            ret = n;
-                        }
-                        n = 0;
-                        b = 0;
+                        a.push(n);
                     }
+                } else {
+                    a.push(n);
                 }
-                _ => {}
             }
-        }
-        if n > 0 {
-            println!("debug {} {}", n, a.len());
-            n -= a.len() as i32;
-            if n > ret {
-                ret = n;
-            }
+            n += 1;
         }
         return ret;
     }
