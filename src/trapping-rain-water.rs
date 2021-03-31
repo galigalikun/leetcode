5b855faf40faf76d877e89e7de4ad2e227770c50
akaishi <galigalikun@gmail.com>
2021-03-31 17:24:13 +0900

    add trapping-rain-water

--- src/trapping-rain-water.rs
+++ src/trapping-rain-water.rs
@@ -1,38 +1,23 @@
 fn main() {
     assert_eq!(Solution::trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
+    assert_eq!(Solution::trap(vec![4, 2, 0, 3, 2, 5]), 9);
 }
 
 pub struct Solution {}
+// https://www.geeksforgeeks.org/trapping-rain-water/
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
         let mut result = 0;
-        for i in 0..height.len() {
-            let p = if i > 0 { height[i - 1] } else { 0 };
-            let c = height[i];
-            /*
-             2 > 0
-             2 > 1
-            */
-            let n = if height.len() > i + 1 {
-                height[i + 1]
-            } else {
-                0
-            };
-            println!("debug {} {} {}", p, c, n);
-            if p > c && n > c {
-                if p > n {
-                    result = n - c;
-                } else {
-                    result = p - c;
-                }
-            } else if p > c {
-                for j in i..height.len() {
-                    if height[j] >= p {
-                        result = p - c;
-                        break;
-                    }
-                }
+        for i in 1..height.len() {
+            let mut left = height[i];
+            for j in 0..i {
+                left = std::cmp::max(left, height[j]);
             }
+            let mut right = height[i];
+            for j in i + 1..height.len() {
+                right = std::cmp::max(right, height[j]);
+            }
+            result = result + (std::cmp::min(left, right) - height[i]);
         }
         return result;
     }
