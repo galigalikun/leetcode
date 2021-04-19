4b1d658fca293ecf508a6b35b676b9e831e91ed5
akaishi <galigalikun@gmail.com>
2021-04-19 20:21:28 +0900

    add balanced-binary-tree

---
+++ src/balanced-binary-tree.rs
@@ -0,0 +1,245 @@
+fn main() {
+    assert_eq!(
+        Solution::is_balanced(Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 9,
+                left: None,
+                right: None
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 20,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 15,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 7,
+                    left: None,
+                    right: None
+                })))
+            })))
+        })))),
+        true
+    );
+
+    assert_eq!(
+        Solution::is_balanced(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None
+            })))
+        })))),
+        false
+    );
+
+    assert_eq!(Solution::is_balanced(None), true);
+
+    assert_eq!(
+        Solution::is_balanced(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                })))
+            })))
+        })))),
+        false
+    );
+
+    /*
+    1
+    /\
+    2   3
+    /\  /
+    4 5 6
+    8
+    */
+    assert_eq!(
+        Solution::is_balanced(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 8,
+                        left: None,
+                        right: None
+                    }))),
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: None
+                }))),
+                right: None
+            })))
+        })))),
+        true
+    );
+
+    assert_eq!(
+        Solution::is_balanced(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None
+            }))),
+            right: None
+        })))),
+        true
+    );
+
+    /*
+    1
+     \
+      3
+     /
+     2
+    */
+
+    assert_eq!(
+        Solution::is_balanced(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+                right: None
+            })))
+        })))),
+        false
+    );
+
+    /*
+    [1,2,2,3,null,null,3,4,null,null,4]
+    1
+    /\
+    2 2
+    /   \
+    3    3
+    /     \
+    4      4
+    */
+
+    assert_eq!(
+        Solution::is_balanced(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    }))),
+                    right: None
+                }))),
+                right: None
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    })))
+                })))
+            })))
+        })))),
+        false
+    );
+}
+
+pub struct Solution {}
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    fn helper(
+root: Option<Rc<RefCell<TreeNode>>>
+    ) -> i32 {
+        if let Some(r) = root {
+            let left = Solution::helper(r.borrow().left.clone());
+            let right = Solution::helper(r.borrow().right.clone());
+            if left == -1 || right == -1 || (left - right).abs() > 1 {
+                return -1;
+            }
+            return 1+std::cmp::max(left, right);
+        }
+
+        return 0;
+    }
+    pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        if Solution::helper(root) == -1 {
+            return false;
+        }
+        return true;
+    }
+}
