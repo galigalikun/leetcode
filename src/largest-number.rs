dfa0817a999a3f01f25dbd2381d813df21727289
akaishi <galigalikun@gmail.com>
2021-05-04 10:52:33 +0900

    add largest-number

--- src/largest-number.rs
+++ src/largest-number.rs
@@ -1,60 +1,49 @@
 fn main() {
-    // assert_eq!(Solution::largest_number(vec![10, 2]), "210".to_string());
-    // assert_eq!(
-    //     Solution::largest_number(vec![3, 30, 34, 5, 9]),
-    //     "9534330".to_string()
-    // );
-    // assert_eq!(Solution::largest_number(vec![1]), "1".to_string());
-    // assert_eq!(Solution::largest_number(vec![10]), "10".to_string());
-    // assert_eq!(
-    //     Solution::largest_number(vec![432, 43243]),
-    //     "43243432".to_string()
-    // );
-    assert_eq!(Solution::largest_number(vec![111311, 1113]), "1113111311".to_string());
+    assert_eq!(Solution::largest_number(vec![10, 2]), "210".to_string());
+    assert_eq!(
+        Solution::largest_number(vec![3, 30, 34, 5, 9]),
+        "9534330".to_string()
+    );
+    assert_eq!(Solution::largest_number(vec![1]), "1".to_string());
+    assert_eq!(Solution::largest_number(vec![10]), "10".to_string());
+    assert_eq!(
+        Solution::largest_number(vec![432, 43243]),
+        "43243432".to_string()
+    );
+    assert_eq!(
+        Solution::largest_number(vec![111311, 1113]),
+        "1113111311".to_string()
+    );
+    assert_eq!(
+        Solution::largest_number(vec![999999991, 9]),
+        "9999999991".to_string()
+    );
+    assert_eq!(Solution::largest_number(vec![0, 0]), "0".to_string());
+    assert_eq!(
+        Solution::largest_number(vec![999999998, 999999997, 999999999]),
+        "999999999999999998999999997".to_string()
+    )
 }
 
 pub struct Solution {}
+// https://lenchen.medium.com/leetcode-179-largest-number-c71272228f2e
 impl Solution {
     pub fn largest_number(nums: Vec<i32>) -> String {
         let mut m_nums = nums;
         m_nums.sort_by(|x, y| {
-            if x.to_string().len() == y.to_string().len() {
-                if x > y {
-                    std::cmp::Ordering::Less
-                } else if x == y {
-                    std::cmp::Ordering::Equal
-                } else {
-                    std::cmp::Ordering::Greater
-                }
-            } else if x.to_string().len() > y.to_string().len() {
-                let yy = format!("{:0<width$}", y, width = x.to_string().len()).parse::<i32>().unwrap();
-                return if x > &yy {
-                    std::cmp::Ordering::Less
-                } else if x == &yy {
-                    std::cmp::Ordering::Equal
-                } else {
-                    std::cmp::Ordering::Greater
-                };
-            } else {
-                let xx = format!("{:0<width$}", x, width = y.to_string().len()).parse::<i32>().unwrap();
-                return if &xx > y {
-                    println!("debug {} {}", xx , y);
-                    std::cmp::Ordering::Less
-                } else if &xx == y {
-                    std::cmp::Ordering::Equal
-                } else {
-                    // 111311
-                    // 111300
-                    println!("debug a {} {}", xx , y);
-                    std::cmp::Ordering::Greater
-                };
-            }
+            let left = format!("{}{}", x, y).parse::<i64>().unwrap();
+            let right = format!("{}{}", y, x).parse::<i64>().unwrap();
+            right.cmp(&left)
         });
 
-        // 1113111311
-        // 1113111113
-        println!("debug list {:?}", m_nums);
+        let s = m_nums
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<String>();
 
-        return m_nums.iter().map(|x| x.to_string()).collect::<String>();
+        if &s[0..1] == "0" {
+            return "0".to_string();
+        }
+        return s;
     }
 }
