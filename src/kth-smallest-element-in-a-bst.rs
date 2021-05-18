ab27091c73c5a2e446c5215975959b7f9ba7bd90
akaishi <galigalikun@gmail.com>
2021-05-18 22:52:36 +0900

    add kth-smallest-element-in-a-bst

---
+++ src/kth-smallest-element-in-a-bst.rs
@@ -0,0 +1,104 @@
+fn main() {
+    assert_eq!(
+        Solution::kth_smallest(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            1
+        ),
+        1
+    );
+
+    assert_eq!(
+        Solution::kth_smallest(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: Some(Rc::new(RefCell::new(TreeNode {
+                            val: 1,
+                            left: None,
+                            right: None
+                        }))),
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            3
+        ),
+        3
+    );
+}
+
+// https://www.geeksforgeeks.org/find-k-th-smallest-element-in-bst-order-statistics-in-bst/
+pub struct Solution {}
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    fn helper(depth:&mut i32, root: Option<Rc<RefCell<TreeNode>>>, k:i32) -> Option<Rc<RefCell<TreeNode>>>{
+        if let Some(r) = root {
+            let left = Solution::helper(depth, r.borrow().left.clone(), k);
+            if  left != None {
+                return left;
+            }
+            *depth +=1;
+
+            if depth == &k {
+                return Some(r);
+            }
+            return Solution::helper(depth, r.borrow().right.clone(), k);
+        }
+
+        return None;
+    }
+    pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        if let Some(r) = Solution::helper(&mut 0, root, k) {
+            return r.borrow().val;
+        }
+        return 0;
+    }
+}
