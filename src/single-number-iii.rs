d0ef9f0cb8c5e2c88e227a3ca6abcaa9313dc9b5
akaishi <galigalikun@gmail.com>
2021-05-24 22:17:15 +0900

    add single-number-iii

---
+++ src/single-number-iii.rs
@@ -0,0 +1,26 @@
+fn main() {
+    assert_eq!(Solution::single_number(vec![1, 2, 1, 3, 2, 5]), vec![3, 5]);
+    assert_eq!(Solution::single_number(vec![-1, 0]), vec![-1, 0]);
+    assert_eq!(Solution::single_number(vec![0,1]), vec![0,1]);
+}
+
+pub struct Solution {}
+use std::collections::HashMap;
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> Vec<i32> {
+        let mut result = vec![];
+        let mut map = HashMap::new();
+        for n in nums {
+            if let Some(m) = map.get_mut(&n) {
+                if m == &1 {
+                    result.retain(|&x| x != n);
+                }
+                *m += 1;
+            } else {
+                map.insert(n, 1);
+                result.push(n);
+            }
+        }
+        return result;
+    }
+}
