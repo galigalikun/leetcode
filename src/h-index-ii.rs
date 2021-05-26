8cb03b49ee4ff41f0132c25057c6c5aa07cd1fb0
akaishi <galigalikun@gmail.com>
2021-05-26 17:30:11 +0900

    add h-index-ii

---
+++ src/h-index-ii.rs
@@ -0,0 +1,22 @@
+fn main() {
+    assert_eq!(Solution::h_index(vec![0, 1, 3, 5, 6]), 3);
+    assert_eq!(Solution::h_index(vec![1,2,100]), 2);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        let mut work = citations;
+        work.sort();
+        let mut result = 0;
+        for i in (0..work.len()).rev() {
+            let cnt = (work.len() - i) as i32;
+            if work[i] >= cnt {
+                result = cnt;
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+}
