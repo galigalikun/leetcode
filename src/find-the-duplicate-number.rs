57b352f1675c03b0c96df1a5d86e2a31bb420ac0
akaishi <galigalikun@gmail.com>
2021-05-29 11:00:46 +0900

    add find-the-duplicate-number

---
+++ src/find-the-duplicate-number.rs
@@ -0,0 +1,22 @@
+fn main() {
+    assert_eq!(Solution::find_duplicate(vec![1, 3, 4, 2, 2]), 2);
+    assert_eq!(Solution::find_duplicate(vec![3, 1, 3, 4, 2]), 3);
+    assert_eq!(Solution::find_duplicate(vec![1,1]), 1);
+    assert_eq!(Solution::find_duplicate(vec![1,1,2]), 1);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn find_duplicate(nums: Vec<i32>) -> i32 {
+        let mut work = nums;
+        work.sort();
+        let mut num: Option<i32> = None;
+        for n in &work {
+            if num == Some(*n) {
+                return *n;
+            }
+            num = Some(*n);
+        }
+        return 0;
+    }
+}
