9595c7cd7e1bbfc6e148fa87ffe6ba0caba27c78
akaishi <galigalikun@gmail.com>
2021-06-22 20:22:38 +0900

    add power-of-three

---
+++ src/power-of-three.rs
@@ -0,0 +1,23 @@
+fn main() {
+    assert_eq!(Solution::is_power_of_three(27), true);
+    assert_eq!(Solution::is_power_of_three(0), false);
+    assert_eq!(Solution::is_power_of_three(9), true);
+    assert_eq!(Solution::is_power_of_three(45), false);
+    assert_eq!(Solution::is_power_of_three(1), true);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn is_power_of_three(n: i32) -> bool {
+        if n == 0 {
+            return false;
+        } else if n == 1 {
+            return true;
+        } else if n == 3 {
+            return true;
+        } else if n % 3 == 0 {
+            return Solution::is_power_of_three(n / 3);
+        }
+        return false;
+    }
+}
