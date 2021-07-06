f04f90ed7fa3e39832cb1926c70450aec4136c59
akaishi <galigalikun@gmail.com>
2021-07-06 20:21:57 +0900

    add power-of-four

---
+++ src/power-of-four.rs
@@ -0,0 +1,21 @@
+fn main() {
+    assert_eq!(Solution::is_power_of_four(16), true);
+    assert_eq!(Solution::is_power_of_four(5), false);
+    assert_eq!(Solution::is_power_of_four(1), true);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn is_power_of_four(n: i32) -> bool {
+        if n == 0 {
+            return false;
+        } else if n == 1 {
+            return true;
+        } else if n == 4 {
+            return true;
+        } else if n % 4 == 0 {
+            return Solution::is_power_of_four(n / 4);
+        }
+        return false;
+    }
+}
