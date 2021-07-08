abd1a025d23695aca4bf2ed7fac4cacb7b19b27c
akaishi <galigalikun@gmail.com>
2021-07-08 19:24:59 +0900

    add reverse-string

---
+++ src/reverse-string.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut s = vec!['h','e','l','l','o'];
+    Solution::reverse_string(&mut s);
+    assert_eq!(s, vec!['o','l','l','e','h']);
+
+    s = vec!['H','a','n','n','a','h'];
+    Solution::reverse_string(&mut s);
+    assert_eq!(s, vec!['h','a','n','n','a','H']);
+}
+
+pub struct Solution{}
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        s.reverse();
+    }
+}
