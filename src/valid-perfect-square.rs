7d7670e9ca9b3b39a48a3b18b8dc10a386817415
akaishi <galigalikun@gmail.com>
2021-07-19 21:27:30 +0900

    add valid-perfect-square

---
+++ src/valid-perfect-square.rs
@@ -0,0 +1,13 @@
+fn main() {
+    assert_eq!(Solution::is_perfect_square(16), true);
+    assert_eq!(Solution::is_perfect_square(14), false);
+    assert_eq!(Solution::is_perfect_square(9), true);
+    assert_eq!(Solution::is_perfect_square(5), false);
+}
+
+pub struct Solution{}
+impl Solution {
+    pub fn is_perfect_square(num: i32) -> bool {
+        return (num as f32).sqrt()%1.0 == 0.0;
+    }
+}
