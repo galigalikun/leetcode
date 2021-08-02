6633fcbb5664c0e5e6d1bfbf053f13c2dcae4a3c
akaishi <galigalikun@gmail.com>
2021-08-02 21:44:48 +0900

    wip mini-parser

---
+++ src/mini-parser.rs
@@ -0,0 +1,19 @@
+fn main() {
+    assert_eq!(Solution::deserialize("324".to_string()), NestedInteger::Int(324));
+    // assert_eq!(Solution::deserialize("[123,[456,[789]]]".to_string()), NestedInteger::List(vec![NestedInteger::Int(123), NestedInteger::List(vec![NestedInteger::Int(456), NestedInteger::List(vec![NestedInteger::Int(789)])])]));
+}
+
+pub struct Solution {}
+#[derive(Debug, PartialEq, Eq)]
+pub enum NestedInteger {
+    Int(i32),
+    List(Vec<NestedInteger>),
+}
+impl Solution {
+    pub fn deserialize(s: String) -> NestedInteger {
+        for c in s.chars() {
+            println!("debug {}", c);
+        }
+        return NestedInteger::Int(324);
+    }
+}
