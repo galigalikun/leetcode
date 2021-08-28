63c6baf45c986dfd4015a8c819919e35f4fbe35a
akaishi <galigalikun@gmail.com>
2021-08-28 12:35:12 +0900

    add longest-palindrome

---
+++ src/longest-palindrome.rs
@@ -0,0 +1,26 @@
+fn main() {
+    assert_eq!(Solution::longest_palindrome("abccccdd".to_string()), 7);
+    assert_eq!(Solution::longest_palindrome("a".to_string()), 1);
+    assert_eq!(Solution::longest_palindrome("bb".to_string()), 2);
+}
+
+pub struct Solution {}
+// https://cheonhyangzhang.gitbooks.io/leetcode-solutions/content/409-longest-palindrome.html
+use std::collections::HashMap;
+impl Solution {
+    pub fn longest_palindrome(s: String) -> i32 {
+        let mut map = HashMap::new();
+        for c in s.chars() {
+            if let Some(m) = map.get_mut(&c) {
+                *m += 1;
+            } else {
+                map.insert(c, 1);
+            }
+        }
+        let mut count = 0;
+        for (_, v) in map {
+            count += v / 2;
+        }
+        return std::cmp::min(s.len() as i32, count * 2 + 1);
+    }
+}
