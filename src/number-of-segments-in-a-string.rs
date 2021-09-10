9ac9129517d4577ce17e527b8bd19543e1c5b329
akaishi <galigalikun@gmail.com>
2021-09-10 21:22:20 +0900

    add number-of-segments-in-a-string

---
+++ src/number-of-segments-in-a-string.rs
@@ -0,0 +1,27 @@
+fn main() {
+    assert_eq!(
+        Solution::count_segments("Hello, my name is John".to_string()),
+        5
+    );
+    assert_eq!(Solution::count_segments("Hello".to_string()), 1);
+    assert_eq!(
+        Solution::count_segments("love live! mu'sic forever".to_string()),
+        4
+    );
+    assert_eq!(Solution::count_segments("".to_string()), 0);
+    assert_eq!(Solution::count_segments("                ".to_string()), 0);
+    assert_eq!(
+        Solution::count_segments(", , , ,        a, eaefa".to_string()),
+        6
+    );
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn count_segments(s: String) -> i32 {
+        return std::cmp::min(
+            s.trim().split(" ").filter(|x| !x.is_empty()).count(),
+            s.trim().len(),
+        ) as i32;
+    }
+}
