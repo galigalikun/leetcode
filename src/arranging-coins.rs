bfa5145d1343f8b7f4cb4cc05113e94cf7b23116
akaishi <galigalikun@gmail.com>
2021-09-18 10:46:52 +0900

    fmt

--- src/arranging-coins.rs
+++ src/arranging-coins.rs
@@ -4,14 +4,14 @@ fn main() {
     assert_eq!(Solution::arrange_coins(10), 4);
 }
 
-pub struct Solution{}
+pub struct Solution {}
 // https://scrapbox.io/rustacean/Arranging_Coins
 impl Solution {
     pub fn arrange_coins(n: i32) -> i32 {
         let mut a = n;
         let mut b = 0;
-        while a - (b+1) >= 0 {
-            b+=1;
+        while a - (b + 1) >= 0 {
+            b += 1;
             a -= b;
         }
 
