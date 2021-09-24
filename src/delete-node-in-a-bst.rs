db14800ce04c4561a14e80648d8edf21d5de4f47
akaishi <galigalikun@gmail.com>
2021-09-24 21:59:23 +0900

    wip delete-node-in-a-bst

---
+++ src/delete-node-in-a-bst.rs
@@ -0,0 +1,151 @@
+fn main() {
+    assert_eq!(
+        Solution::delete_node(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: None,
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 7,
+                        left: None,
+                        right: None
+                    })))
+                })))
+            }))),
+            3
+        ),
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 5,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 4,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+                right: None
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 6,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 7,
+                    left: None,
+                    right: None
+                })))
+            })))
+        })))
+    );
+
+    assert_eq!(
+        Solution::delete_node(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: None,
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 7,
+                        left: None,
+                        right: None
+                    })))
+                })))
+            }))),
+            0
+        ),
+        None
+    );
+
+    assert_eq!(Solution::delete_node(None, 0), None);
+}
+
+pub struct Solution {}
+// https://www.techiedelight.com/deletion-from-bst/
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn delete_node(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        key: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut curr = root.clone();
+        let mut parent:Option<Rc<RefCell<TreeNode>>> = None;
+        loop {
+            if let Some(c) = curr.clone() {
+                let v = c.borrow().val;
+                if v == key {
+                    break;
+                }
+                parent = curr;
+                if key < v {
+                    curr = c.borrow().left.clone();
+                } else {
+                    curr = c.borrow().right.clone();
+                }
+            } else {
+                break;
+            }
+        }
+        if let Some(c) = curr {
+            match (c.borrow().left.clone(), c.borrow().right.clone()) {
+                ((Some(l), Some(r))) => {
+
+                },
+                (None, None) => {
+
+                },
+                _ => {
+                    // todo
+                }
+            }
+        } else {
+            return root;
+        }
+        return None;
+    }
+}
