c4e9d337fa4c8c64abca3d8cf1aac3dcb579b3fa
akaishi <galigalikun@gmail.com>
2021-09-27 21:29:08 +0900

    add minimum-moves-to-equal-array-elements

---
+++ src/minimum-moves-to-equal-array-elements.rs
@@ -0,0 +1,18 @@
+fn main() {
+    assert_eq!(Solution::min_moves(vec![1, 2, 3]), 3);
+    assert_eq!(Solution::min_moves(vec![1, 1, 1]), 0);
+}
+
+pub struct Solution {}
+// https://www.geeksforgeeks.org/minimum-number-increment-operations-make-array-elements-equal/
+impl Solution {
+    pub fn min_moves(nums: Vec<i32>) -> i32 {
+        let mut min = std::i32::MAX;
+        let mut sum = 0;
+        for i in 0..nums.len() {
+            sum += nums[i];
+            min = std::cmp::min(min, nums[i]);
+        }
+        return sum - (nums.len() as i32 * min);
+    }
+}
