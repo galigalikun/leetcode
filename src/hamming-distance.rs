0f9cc757ee06e62fad9354113fa8a67f5b8b8989
akaishi <galigalikun@gmail.com>
2021-10-05 20:20:38 +0900

    add hamming-distance

---
+++ src/hamming-distance.rs
@@ -0,0 +1,14 @@
+fn main() {
+    assert_eq!(Solution::hamming_distance(1, 4), 2);
+    assert_eq!(Solution::hamming_distance(3, 1), 1);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn hamming_distance(x: i32, y: i32) -> i32 {
+        return format!("{0:b}", x ^ y)
+            .chars()
+            .filter(|&x| x == '1')
+            .count() as i32;
+    }
+}
