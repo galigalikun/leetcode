1ccd729250ff9d62747c5e8ccef5162833c53286
akaishi <galigalikun@gmail.com>
2021-10-07 19:40:41 +0900

    add island-perimeter

---
+++ src/island-perimeter.rs
@@ -0,0 +1,48 @@
+fn main() {
+    assert_eq!(
+        Solution::island_perimeter(vec![
+            vec![0, 1, 0, 0],
+            vec![1, 1, 1, 0],
+            vec![0, 1, 0, 0],
+            vec![1, 1, 0, 0]
+        ]),
+        16
+    );
+    assert_eq!(Solution::island_perimeter(vec![vec![1]]), 4);
+    assert_eq!(Solution::island_perimeter(vec![vec![1, 0]]), 4);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+        let mut result = 0;
+        for x in 0..grid.len() {
+            for y in 0..grid[x].len() {
+                if grid[x][y] == 1 {
+                    result += 4;
+                    if x > 0 {
+                        if grid[x - 1][y] == 1 {
+                            result -= 1;
+                        }
+                    }
+                    if x < grid.len() - 1 {
+                        if grid[x + 1][y] == 1 {
+                            result -= 1;
+                        }
+                    }
+                    if y > 0 {
+                        if grid[x][y - 1] == 1 {
+                            result -= 1;
+                        }
+                    }
+                    if y < grid[x].len() - 1 {
+                        if grid[x][y + 1] == 1 {
+                            result -= 1;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
