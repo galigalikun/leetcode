5094889f0299f470d7836dd9b82decc8423a625e
akaishi <galigalikun@gmail.com>
2021-10-12 19:51:31 +0900

    add implement-rand10-using-rand7

---
+++ src/implement-rand10-using-rand7.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("debug {}", Solution::rand10());
+}
+
+pub struct Solution {}
+use rand::{Rng, thread_rng};
+fn rand7() -> i32 {
+    return thread_rng().gen_range(1..=7);
+}
+/**
+ * The rand7() API is already defined for you.
+ * @return a random integer in the range 1 to 7
+ * fn rand7() -> i32;
+ */
+
+impl Solution {
+    pub fn rand10() -> i32 {
+        return 1+(1..=10).map(|_x| rand7()).fold(0, |sum, a| sum + a)%10;
+    }
+}
