38a6ba9338ae938fd1a77784db4239b5c1e4381f
akaishi <galigalikun@gmail.com>
2021-10-17 10:36:56 +0900

    add number-complement

---
+++ src/number-complement.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::find_complement(5), 2);
+    assert_eq!(Solution::find_complement(1), 0);
+}
+
+pub struct Solution {}
+impl Solution {
+    pub fn find_complement(num: i32) -> i32 {
+        return num ^ (2_i32.pow(format!("{:0>b}", num).len() as u32) - 1);
+    }
+}
