ddfed0a9e3a1d1b1a74651e9d968a74c22c4f782
akaishi <galigalikun@gmail.com>
2021-10-20 20:07:31 +0900

    fmt

--- src/generate-random-point-in-a-circle.rs
+++ src/generate-random-point-in-a-circle.rs
@@ -7,27 +7,25 @@ struct Solution {
     y_center: f64,
 }
 
-
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl Solution {
-
     fn new(radius: f64, x_center: f64, y_center: f64) -> Self {
-        Solution{
+        Solution {
             rng: thread_rng(),
-            radius:radius,
-            x_center:x_center,
-            y_center:y_center,
+            radius: radius,
+            x_center: x_center,
+            y_center: y_center,
         }
     }
 
     fn rand_point(&mut self) -> Vec<f64> {
         let r = self.rng.gen_range(0f64..self.radius);
-        let a = self.rng.gen_range(0f64..2f64*std::f64::consts::PI);
+        let a = self.rng.gen_range(0f64..2f64 * std::f64::consts::PI);
         println!("r a {} {}", r, a);
-        return vec![r*a.cos()+self.x_center, r*a.sin()+self.y_center];
+        return vec![r * a.cos() + self.x_center, r * a.sin() + self.y_center];
     }
 }
 
@@ -41,7 +39,7 @@ fn main() {
     let ret_1: Vec<f64> = obj.rand_point();
     println!("debug {:?}", ret_1);
 
-    let mut obj_2 = Solution::new(0.01,-73839.1,-3289891.3);
+    let mut obj_2 = Solution::new(0.01, -73839.1, -3289891.3);
     let ret_2: Vec<f64> = obj_2.rand_point();
     println!("debug {:?}", ret_2);
 }
