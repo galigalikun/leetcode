4d7e8c4655495c53e03cbc29d8cafea1f42fe67d
akaishi <galigalikun@gmail.com>
2022-01-05 19:44:05 +0900

    add array-nesting

--- problems/array-nesting/src/main.rs
+++ problems/array-nesting/src/main.rs
@@ -1,19 +1,30 @@
 fn main() {
     assert_eq!(Solution::array_nesting(vec![5, 4, 0, 3, 1, 6, 2]), 4);
     assert_eq!(Solution::array_nesting(vec![0, 1, 2]), 1);
+    assert_eq!(Solution::array_nesting(vec![0, 2, 1]), 2);
 }
 
+// https://studyalgorithms.com/array/array-nesting/
 struct Solution {}
 impl Solution {
     pub fn array_nesting(nums: Vec<i32>) -> i32 {
-        let mut idx = 0;
-        let mut ans = 1;
-        loop {
-            idx = nums[idx as usize];
-            if idx == 0 {
-                return ans;
+        let mut visited = vec![false; nums.len()];
+        let mut ans = 0;
+        for i in 0..nums.len() {
+            if !visited[i] {
+                let mut start = nums[i];
+                let mut count = 0;
+                loop {
+                    start = nums[start as usize];
+                    count += 1;
+                    visited[start as usize] = true;
+                    if start == nums[i] {
+                        break;
+                    }
+                }
+                ans = std::cmp::max(ans, count);
             }
-            ans += 1;
         }
+        return ans;
     }
 }
