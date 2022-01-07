fc0451a6161e87ad575b0698d41bbe7717904676
akaishi <galigalikun@gmail.com>
2022-01-07 22:59:24 +0900

    add permutation-in-string

--- problems/permutation-in-string/src/main.rs
+++ problems/permutation-in-string/src/main.rs
@@ -13,14 +13,33 @@ fn main() {
     );
 }
 
+// https://just4once.gitbooks.io/leetcode-notes/content/leetcode/two-pointers/567-permutation-in-string.html
 struct Solution {}
 impl Solution {
     pub fn check_inclusion(s1: String, s2: String) -> bool {
-        if s2.contains(&s1) {
-            return true;
+        let mut map = vec![0; 128];
+        for c in s1.chars() {
+            map[c as usize] += 1;
         }
-        if s2.contains(&s1.chars().rev().collect::<String>()) {
-            return true;
+        let mut count = s1.len();
+        let (mut left, mut right) = (0, 0);
+        while right < s2.len() {
+            if map[s2.chars().nth(right).unwrap() as usize] > 0 {
+                count -= 1;
+            }
+            map[s2.chars().nth(right).unwrap() as usize] -= 1;
+            right += 1;
+
+            while count == 0 {
+                if (right - left) == s1.len() {
+                    return true;
+                }
+                map[s2.chars().nth(left).unwrap() as usize] += 1;
+                if map[s2.chars().nth(left).unwrap() as usize] > 0 {
+                    count += 1;
+                }
+                left += 1;
+            }
         }
         return false;
     }
