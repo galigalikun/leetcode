737e713d6b987cb3821dddd1ce7b2af5991e7175
akaishi <galigalikun@gmail.com>
2022-01-09 12:22:43 +0900

    add subtree-of-another-tree

--- problems/subtree-of-another-tree/src/main.rs
+++ problems/subtree-of-another-tree/src/main.rs
@@ -101,14 +101,49 @@ impl TreeNode {
         }
     }
 }
+
+// https://qiita.com/redpeaks33/items/8366f3a4f8b5ed894980
 struct Solution {}
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
+    fn helper(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        sub_root: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        return match (root, sub_root) {
+            (Some(r), Some(s)) => {
+                if r.borrow().val != s.borrow().val {
+                    false
+                } else {
+                    Solution::helper(r.borrow().left.clone(), s.borrow().left.clone())
+                        & Solution::helper(r.borrow().right.clone(), s.borrow().right.clone())
+                }
+            }
+            (Some(_r), None) => false,
+            (None, Some(_s)) => false,
+            (None, None) => true,
+        };
+    }
     pub fn is_subtree(
         root: Option<Rc<RefCell<TreeNode>>>,
         sub_root: Option<Rc<RefCell<TreeNode>>>,
     ) -> bool {
-        return false;
+        return match (root.clone(), sub_root.clone()) {
+            (Some(r), Some(s)) => {
+                if r.borrow().val == s.borrow().val {
+                    if Solution::helper(root.clone(), sub_root.clone()) {
+                        true
+                    } else {
+                        Solution::is_subtree(r.borrow().left.clone(), sub_root.clone())
+                            || Solution::is_subtree(r.borrow().right.clone(), sub_root.clone())
+                    }
+                } else {
+                    Solution::is_subtree(r.borrow().left.clone(), sub_root.clone())
+                        || Solution::is_subtree(r.borrow().right.clone(), sub_root.clone())
+                }
+            }
+            _ => false,
+        };
     }
 }
