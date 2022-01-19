65d85a4cef575846ca6ac1a585d5334ed5462253
akaishi <galigalikun@gmail.com>
2022-01-19 09:16:44 +0900

    add n-ary-tree-preorder-traversal

--- problems/n-ary-tree-preorder-traversal/main.cc
+++ problems/n-ary-tree-preorder-traversal/main.cc
@@ -22,8 +22,41 @@ public:
 
 
 class Solution {
-public:
-    std::vector<int> preorder(Node* root) {
+    std::vector<int> ans;
+    void helper(Node *root)
+    {
+        if (root != nullptr) {
+            ans.push_back(root->val);
+            for (const auto &c : root->children)
+            {
+                this->helper(c);
+            }
+        }
+    }
 
+public:
+    Solution() :ans(0) {}
+    std::vector<int> preorder(Node* root) {
+        this->helper(root);
+        return ans;
     }
 };
+
+int main() {
+    auto s = new Solution();
+    auto ans = s->preorder(new Node(1, {
+        new Node(3, {
+            new Node(5),
+            new Node(6)
+        }),
+        new Node(2),
+        new Node(4)
+    }));
+    std::cout << "[";
+    for (auto a : ans)
+    {
+        std::cout << a << ",";
+    }
+    std::cout << "]" << std::endl;
+    return 0;
+}
