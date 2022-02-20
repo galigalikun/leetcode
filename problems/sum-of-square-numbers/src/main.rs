99c36bd21f109e44aa2952860e495b2bec7af2b5
akaishi <galigalikun@gmail.com>
2022-02-20 11:22:11 +0900

    turning sum-of-square-numbers

--- problems/sum-of-square-numbers/src/main.rs
+++ problems/sum-of-square-numbers/src/main.rs
@@ -4,16 +4,21 @@ fn main() {
     assert_eq!(Solution::judge_square_sum(3), false);
     assert_eq!(Solution::judge_square_sum(2), true);
     assert_eq!(Solution::judge_square_sum(5487023), false);
+    assert_eq!(Solution::judge_square_sum(10000000), true);
+    assert_eq!(Solution::judge_square_sum(2147482647), true);
 }
 
 struct Solution {}
 impl Solution {
     pub fn judge_square_sum(c: i32) -> bool {
-        let n = (c as f64).sqrt();
-        for a in (0..=n as i32).rev() {
-            for b in 0..=n as i32 {
-                if a.pow(2) + b.pow(2) == c {
+        let n = (c as f64).sqrt() as i64;
+        for a in (0..=n).rev() {
+            for b in 0..=n {
+                let c2 = a.pow(2) + b.pow(2);
+                if c2 == c as i64 {
                     return true;
+                } else if c2 > c as i64 {
+                    break;
                 }
             }
         }
