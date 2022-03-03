0ebcc10cdf055e1a22f2958058df852352a404e9
akaishi <galigalikun@gmail.com>
2022-03-03 22:56:39 +0900

    wip palindromic-substrings

---
+++ problems/palindromic-substrings/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::count_substrings("abc".to_string()), 3);
+    assert_eq!(Solution::count_substrings("aaa".to_string()), 6);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn count_substrings(s: String) -> i32 {
+        return 0;
+    }
+}
