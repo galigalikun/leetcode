36b842555af28baa10ee59dc1bd3d61cc277532d
akaishi <galigalikun@gmail.com>
2022-03-04 19:39:21 +0900

    add palindromic-substrings

--- problems/palindromic-substrings/src/main.rs
+++ problems/palindromic-substrings/src/main.rs
@@ -3,9 +3,20 @@ fn main() {
     assert_eq!(Solution::count_substrings("aaa".to_string()), 6);
 }
 
-struct Solution{}
+struct Solution {}
 impl Solution {
     pub fn count_substrings(s: String) -> i32 {
-        return 0;
+        let mut ans = 0;
+        for i in 0..s.len() {
+            ans += 1;
+            for j in 1..=i {
+                if &s[i - j..i + 1].to_string()
+                    == &s[i - j..i + 1].chars().rev().collect::<String>()
+                {
+                    ans += 1;
+                }
+            }
+        }
+        return ans;
     }
 }
