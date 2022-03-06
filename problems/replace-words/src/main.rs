12921d922be3caa81b2e62c0498a423a23d383f4
akaishi <galigalikun@gmail.com>
2022-03-06 09:46:48 +0900

    add replace-words

--- problems/replace-words/src/main.rs
+++ problems/replace-words/src/main.rs
@@ -25,17 +25,32 @@ fn main() {
         ),
         "a a a a a a a a bbb baba a"
     );
+    assert_eq!(
+        Solution::replace_words(
+            vec![
+                "catt".to_string(),
+                "cat".to_string(),
+                "bat".to_string(),
+                "rat".to_string()
+            ],
+            "the cattle was rattled by the battery".to_string()
+        ),
+        "the cat was rat by the bat"
+    );
 }
 
 struct Solution {}
 impl Solution {
     pub fn replace_words(dictionary: Vec<String>, sentence: String) -> String {
+        let mut dd = dictionary;
+        dd.sort();
         let mut ans = String::new();
         for s in sentence.split(" ") {
             let mut p = s;
-            for d in &dictionary {
+            for d in &dd {
                 if s.len() >= d.len() && &s[0..d.len()] == d {
                     p = d;
+                    break;
                 }
             }
             ans = format!("{} {}", ans, p);
