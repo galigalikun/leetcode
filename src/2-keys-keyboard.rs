f3b53b56cf895e9e18736f692933d3b75f722270
akaishi <galigalikun@gmail.com>
2022-03-12 08:44:55 +0900

    add 2-keys-keyboard

--- src/2-keys-keyboard.rs
+++ src/2-keys-keyboard.rs
@@ -3,8 +3,18 @@ fn main() {
     assert_eq!(Solution::min_steps(1), 0);
 }
 
-struct Solution{}
+// https://twchen.gitbook.io/leetcode/dynamic-programming/2-keys-keyboard
+struct Solution {}
 impl Solution {
+    fn helper(n: i32, m: i32) -> i32 {
+        if n < m {
+            return 1000;
+        } else if n > m {
+            return Solution::helper(n - m, m) + 1;
+        } else {
+            return Solution::min_steps(n) + 1;
+        }
+    }
     pub fn min_steps(n: i32) -> i32 {
         // A 1 -> 0
         // AA 2 -> 2
@@ -18,6 +28,19 @@ impl Solution {
         // AAAAAAAAAA 10 -> 7
         // AAAAAAAAAAA 11 -> 11
         // AAAAAAAAAAAA 12 -> 7 3*3+3
-        return 0;
+        if n == 1 {
+            return 0;
+        }
+        let mut ans = 1000;
+        let mut m = 1;
+        loop {
+            let step = Solution::helper(n - m, m);
+            ans = std::cmp::min(ans, step);
+            m += 1;
+            if 2 * m > n {
+                break;
+            }
+        }
+        return ans + 1;
     }
 }
