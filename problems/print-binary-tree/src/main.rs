db10600219520aa75afdd08b2b3ce88a46f1418c
akaishi <galigalikun@gmail.com>
2022-03-19 09:58:46 +0900

    wip

--- problems/print-binary-tree/src/main.rs
+++ problems/print-binary-tree/src/main.rs
@@ -36,6 +36,36 @@ fn main() {
             ["", "", "4", "", "", "", ""]
         ]
     );
+
+    assert_eq!(
+        Solution::print_tree(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    }))),
+                    right: None
+                }))),
+                right: None
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: None,
+                right: None
+            })))
+        })))),
+        vec![
+            ["", "", "", "", "", "", "", "1", "", "", "", "", "", "", ""],
+            ["", "", "", "2", "", "", "", "", "", "", "", "5", "", "", ""],
+            ["", "3", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+            ["4", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+        ]
+    );
 }
 
 struct Solution {}
@@ -60,15 +90,19 @@ impl TreeNode {
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
-    fn helper(result: &mut Vec<Vec<String>>, depth:usize, root: Option<Rc<RefCell<TreeNode>>>) {
+    fn helper(
+        result: &mut Vec<Vec<String>>,
+        depth: usize,
+        idx: usize,
+        root: Option<Rc<RefCell<TreeNode>>>,
+    ) {
         if let Some(r) = root {
             if result.len() == depth {
-                result.push(vec![format!("{}", r.borrow().val)]);
-            } else {
-                result[depth].push(format!("{}", r.borrow().val));
+                result.push(vec![String::new(); 2_usize.pow(depth as u32)]);
             }
-            Solution::helper(result, depth+1, r.borrow().left.clone());
-            Solution::helper(result, depth+1, r.borrow().right.clone());
+            result[depth][idx] = format!("{}", r.borrow().val);
+            Solution::helper(result, depth + 1, idx * 2, r.borrow().left.clone());
+            Solution::helper(result, depth + 1, idx * 2 + 1, r.borrow().right.clone());
         }
     }
     pub fn print_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<String>> {
@@ -80,16 +114,39 @@ impl Solution {
         // f(n) = 2*f(n-1) + 1
         // f(n) = 2^(n+1) - 1
         let mut result = vec![];
-        Solution::helper(&mut result, 0, root);
+        Solution::helper(&mut result, 0, 0, root);
         let size = 2_usize.pow(result.len() as u32) - 1;
-        let mut ans = vec![];
-        for i in 0..result.len() {
-            ans.push(vec![String::new(); size]);
-            for j in 0..result[i].len() {
-                ans[i][j] = result[i][j].clone();
+        let mut ans = vec![vec![String::new(); size]; result.len()];
+        let mut block = vec![0; size];
+        for i in (0..result.len()).rev() {
+            for j in 0..result[i].len() / 2 {
+                let mut idx = j * 2;
+                loop {
+                    if block[idx] == 0 {
+                        ans[i][idx] = result[i][j].clone();
+                        block[idx] = 1;
+                        break;
+                    }
+                    idx = idx + 1;
+                }
+            }
+            let mut idx = size - 1;
+            for j in (result[i].len() / 2..result[i].len()).rev() {
+                loop {
+                    if block[idx] == 0 {
+                        ans[i][idx] = result[i][j].clone();
+                        block[idx] = 1;
+                        if idx > 1 {
+                            idx -= 2;
+                        } else {
+                            idx = 0;
+                        }
+                        break;
+                    }
+                    idx = idx - 1;
+                }
             }
         }
-        println!("debug {:?} {} {}", result, result.len(), size);
         return ans;
     }
 }
