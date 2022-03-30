b567a7bf4d1ce77d75444872367e3899a60f3534
akaishi <galigalikun@gmail.com>
2022-03-31 05:34:04 +0900

    wip non-decreasing-array

---
+++ problems/non-decreasing-array/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::check_possibility(vec![4,2,3]), true);
+    assert_eq!(Solution::check_possibility(vec![4,2,1]), false);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn check_possibility(nums: Vec<i32>) -> bool {
+        return false;
+    }
+}
