223d94d45a1e6b58b11ac4d298debe78e4fbe100
akaishi <galigalikun@gmail.com>
2022-03-31 19:21:19 +0900

    wip non-decreasing-array

--- problems/non-decreasing-array/src/main.rs
+++ problems/non-decreasing-array/src/main.rs
@@ -1,11 +1,22 @@
 fn main() {
-    assert_eq!(Solution::check_possibility(vec![4,2,3]), true);
-    assert_eq!(Solution::check_possibility(vec![4,2,1]), false);
+    assert_eq!(Solution::check_possibility(vec![4, 2, 3]), true);
+    assert_eq!(Solution::check_possibility(vec![4, 2, 1]), false);
 }
 
-struct Solution{}
+struct Solution {}
 impl Solution {
     pub fn check_possibility(nums: Vec<i32>) -> bool {
+        let mut n = nums[0];
+        for i in 1..nums.len() {
+            if nums[i] < n {
+                if i == 1 || nums[i - 1] <= nums[i] {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            n = nums[i];
+        }
         return false;
     }
 }
