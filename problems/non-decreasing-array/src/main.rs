063da2b3fc97377aa077ad588f7b102a75224473
akaishi <galigalikun@gmail.com>
2022-04-01 22:02:49 +0900

    add non-decreasing-array

--- problems/non-decreasing-array/src/main.rs
+++ problems/non-decreasing-array/src/main.rs
@@ -3,20 +3,24 @@ fn main() {
     assert_eq!(Solution::check_possibility(vec![4, 2, 1]), false);
 }
 
+// https://dev.to/seanpgallivan/solution-non-decreasing-array-1m5c
 struct Solution {}
 impl Solution {
     pub fn check_possibility(nums: Vec<i32>) -> bool {
-        let mut n = nums[0];
+        let mut err = 0;
         for i in 1..nums.len() {
-            if nums[i] < n {
-                if i == 1 || nums[i - 1] <= nums[i] {
-                    return true;
-                } else {
+            if nums[i] < nums[i - 1] {
+                if err > 0
+                    || (i > 1
+                        && i < nums.len() - 1
+                        && nums[i - 2] > nums[i]
+                        && nums[i + 1] < nums[i - 1])
+                {
                     return false;
                 }
+                err += 1;
             }
-            n = nums[i];
         }
-        return false;
+        return true;
     }
 }
