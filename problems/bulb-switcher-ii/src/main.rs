6540d341d87274d54d6987de9e5eb67dbab7ed9b
akaishi <galigalikun@gmail.com>
2022-04-09 08:14:06 +0900

    add bulb-switcher-ii

--- problems/bulb-switcher-ii/src/main.rs
+++ problems/bulb-switcher-ii/src/main.rs
@@ -5,18 +5,16 @@ fn main() {
     assert_eq!(Solution::flip_lights(1, 2), 2);
 }
 
+// https://baihuqian.github.io/2018-07-31-bulb-switcher-ii/
 struct Solution {}
 impl Solution {
     pub fn flip_lights(n: i32, presses: i32) -> i32 {
-        return match n {
-            1 | 2 => presses + n,
-            _ => {
-                let mut res = n;
-                for i in 1..=presses {
-                    res += i;
-                }
-                res
-            }
+        let key = (std::cmp::min(n, 3) - 1) as usize;
+        return match presses {
+            0 => 1,
+            1 => vec![2, 3, 4][key],
+            2 => vec![2, 4, 7][key],
+            _ => vec![2, 4, 8][key],
         };
     }
 }
