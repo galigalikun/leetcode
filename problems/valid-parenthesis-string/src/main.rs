b29256eeabca6eb5b12d76485fce794f8e35d73c
akaishi <galigalikun@gmail.com>
2022-04-19 06:44:59 +0900

    check valid-parenthesis-string

--- problems/valid-parenthesis-string/src/main.rs
+++ problems/valid-parenthesis-string/src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
     assert_eq!(Solution::check_valid_string("())".to_string()), true);
+    assert_eq!(Solution::check_valid_string("(*)".to_string()), true);
     assert_eq!(Solution::check_valid_string("(*))".to_string()), true);
 }
 
-struct Solution{}
+struct Solution {}
 impl Solution {
     pub fn check_valid_string(s: String) -> bool {
+        for i in 0..s.len() {
+            if s.chars().nth(i) == Some('*') {
+                let mut s = s.clone();
+                s.remove(i);
+                if Solution::check_valid_string(s) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
