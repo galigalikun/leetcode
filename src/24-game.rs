401a433c4aea7376cce5d202d9f372ecb4a1f076
akaishi <galigalikun@gmail.com>
2022-04-20 20:59:28 +0900

    wip 24-game

---
+++ src/24-game.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::judge_point24(vec![4,1,8,7]), true);
+    assert_eq!(Solution::judge_point24(vec![1,2,1,2]), false);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn judge_point24(cards: Vec<i32>) -> bool {
+        return false;
+    }
+}
