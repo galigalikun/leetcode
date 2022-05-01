1e91c8feb4c389274ed026e69e1d87a68e7ec4ca
akaishi <galigalikun@gmail.com>
2022-05-01 17:08:17 +0900

    add repeated-string-match

--- problems/repeated-string-match/src/main.rs
+++ problems/repeated-string-match/src/main.rs
@@ -1,58 +1,57 @@
 fn main() {
-    assert_eq!(Solution::repeated_string_match("abcd".to_string(), "cdabcdab".to_string()), 3);
-    assert_eq!(Solution::repeated_string_match("a".to_string(), "aa".to_string()), 2);
+    // assert_eq!(Solution::repeated_string_match("abcd".to_string(), "cdabcdab".to_string()), 3);
+    // assert_eq!(Solution::repeated_string_match("a".to_string(), "aa".to_string()), 2);
+    assert_eq!(Solution::repeated_string_match("baaabbbaba".to_string(), "baaabbbababaaabbbababaaabbbababaaabbbababaaabbbababaaabbbababaaabbbababaaabbbababaaabbbababaaabbbaba".to_string()), 10);
 }
 
-struct Solution{}
+// https://jpdebug.com/p/2660110
+struct Solution {}
 impl Solution {
     pub fn repeated_string_match(a: String, b: String) -> i32 {
         if a.len() >= b.len() {
-            let bb:&str = &b;
-            if (&a).chars().collect::<Vec<_>>().contains(b) {
+            if a.contains(&b) {
                 return 1;
             } else {
                 let tmp1 = format!("{}{}", a, a);
-                if tmp1.chars().collect::<Vec<_>>().contains(&b) {
+                if tmp1.contains(&b) {
                     return 2;
                 } else {
-                    return -1
+                    return -1;
+                }
             }
         } else {
-            /*
-            let num1 = b.len()/a.len();
-            let num2 = b.len()%a.len();
-            let mut tmp2 = a;
+            let num1 = (b.len() / a.len()) as i32;
+            let num2 = (b.len() % a.len()) as i32;
+            let mut tmp2 = a.clone();
             if num2 != 0 {
-                for i in 1..=num1 {
+                for _i in 1..=num1 {
                     tmp2 = format!("{}{}", tmp2, a);
                 }
-                if tmp2.chars().collect::<vec<_>>().contains(&b) {
+                if tmp2.contains(&b) {
                     return num1 + 1;
                 } else {
                     tmp2 = format!("{}{}", tmp2, a);
-                    if tmp2.chars().collect::<vec<_>>().contains(&b) {
+                    if tmp2.contains(&b) {
                         return num1 + 2;
                     } else {
                         return -1;
                     }
                 }
             } else {
-                for i in 1..num1 {
+                for _i in 1..num1 {
                     tmp2 = format!("{}{}", tmp2, a);
-                    if tmp2.chars().collect::<vec<_>>().contains(&b) {
-                        return num1;
+                }
+                if tmp2.contains(&b) {
+                    return num1;
+                } else {
+                    tmp2 = format!("{}{}", tmp2, a);
+                    if tmp2.contains(&b) {
+                        return num1 + 1;
                     } else {
-                        tmp2 = format!("{}{}", tmp2, a);
-                        if tmp2.chars().collect::<vec<_>>().contains(&b) {
-                            return num1 + 1;
-                        } else {
-                            return -1;
-                        }
+                        return -1;
                     }
                 }
             }
-            */
         }
-        return -1;
     }
 }
