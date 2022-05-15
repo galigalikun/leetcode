b9dcd72fd0b53bf9788a21ca6f489b305d9c1c02
akaishi <galigalikun@gmail.com>
2022-05-15 23:33:31 +0900

    wip

--- problems/degree-of-an-array/src/main.rs
+++ problems/degree-of-an-array/src/main.rs
@@ -8,21 +8,31 @@ use std::collections::HashMap;
 impl Solution {
     pub fn find_shortest_sub_array(nums: Vec<i32>) -> i32 {
         let mut count = HashMap::new();
+        let mut left = HashMap::new();
+        let mut right = HashMap::new();
         let mut max_count = 0;
+        let mut result = 0;
         for num in nums.clone() {
             *count.entry(num).or_insert(0) += 1;
             if *count.get(&num).unwrap() > max_count {
                 max_count = *count.get(&num).unwrap();
             }
+            *left.entry(num).or_insert(0) += 1;
+            *right.entry(num).or_insert(0) += 1;
         }
-        println!("debug {:?} {}", count, max_count);
         let mut min_length = nums.len() as i32;
-        for num in count.keys() {
-            if *count.get(num).unwrap() == max_count {
-                min_length = std::cmp::min(min_length, *num+1);
+        for (key, value) in count.iter() {
+            if *value == max_count {
+                let left_count = *left.get(key).unwrap();
+                let right_count = *right.get(key).unwrap();
+                let length = right_count - left_count + 1;
+                if length < min_length {
+                    min_length = length;
+                    result = max_count;
+                }
             }
         }
 
-        min_length
+        result
     }
 }
