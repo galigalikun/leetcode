acb96b8d6560f55c1333f77742b33f3ec9552209
akaishi <galigalikun@gmail.com>
2022-05-16 20:56:27 +0900

    add degree-of-an-array

--- problems/degree-of-an-array/src/main.rs
+++ problems/degree-of-an-array/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    assert_eq!(Solution::find_shortest_sub_array(vec![1,2,2,3,1]), 2);
-    assert_eq!(Solution::find_shortest_sub_array(vec![1,2,2,3,1,4,2]), 6);
+    assert_eq!(Solution::find_shortest_sub_array(vec![1, 2, 2, 3, 1]), 2);
+    assert_eq!(
+        Solution::find_shortest_sub_array(vec![1, 2, 2, 3, 1, 4, 2]),
+        6
+    );
 }
 
-struct Solution{}
+// https://chih-hsien.gitbooks.io/mysolution/content/degree-of-an-array.html
+struct Solution {}
 use std::collections::HashMap;
 impl Solution {
     pub fn find_shortest_sub_array(nums: Vec<i32>) -> i32 {
@@ -11,28 +15,24 @@ impl Solution {
         let mut left = HashMap::new();
         let mut right = HashMap::new();
         let mut max_count = 0;
-        let mut result = 0;
-        for num in nums.clone() {
-            *count.entry(num).or_insert(0) += 1;
-            if *count.get(&num).unwrap() > max_count {
-                max_count = *count.get(&num).unwrap();
+        for (i, num) in nums.iter().enumerate() {
+            if !count.contains_key(&num) {
+                *left.entry(*num).or_insert(i) = i;
             }
-            *left.entry(num).or_insert(0) += 1;
-            *right.entry(num).or_insert(0) += 1;
+            *count.entry(num).or_insert(0) += 1;
+            *right.entry(*num).or_insert(i) = i;
+            max_count = std::cmp::max(max_count, *count.get(&num).unwrap());
         }
-        let mut min_length = nums.len() as i32;
+        let mut result = nums.len() as i32;
         for (key, value) in count.iter() {
             if *value == max_count {
-                let left_count = *left.get(key).unwrap();
-                let right_count = *right.get(key).unwrap();
-                let length = right_count - left_count + 1;
-                if length < min_length {
-                    min_length = length;
-                    result = max_count;
-                }
+                result = std::cmp::min(
+                    result,
+                    (right.get(key).unwrap() - left.get(key).unwrap() + 1) as i32,
+                );
             }
         }
 
-        result
+        return result;
     }
 }
