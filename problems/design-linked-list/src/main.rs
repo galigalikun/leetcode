a56bcd16419c2e415e5121fc069915d91adcf080
akaishi <galigalikun@gmail.com>
2022-05-29 10:54:39 +0900

    wip design-linked-list

--- problems/design-linked-list/src/main.rs
+++ problems/design-linked-list/src/main.rs
@@ -1,23 +1,21 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
 struct MyLinkedList {
     head: Option<Box<Node>>,
 }
 
+#[derive(PartialEq, Eq, Clone, Debug)]
 struct Node {
     next: Option<Box<Node>>,
     val: i32,
 }
-
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl MyLinkedList {
-
     fn new() -> Self {
-        MyLinkedList {
-            head: None,
-        }
+        MyLinkedList { head: None }
     }
 
     fn get(&self, index: i32) -> i32 {
@@ -27,11 +25,17 @@ impl MyLinkedList {
             node = &node.as_ref().unwrap().next;
             i += 1;
         }
-        return node.as_ref().unwrap().val;
+        return node
+            .as_ref()
+            .unwrap_or(&Box::new(Node {
+                next: None,
+                val: -1,
+            }))
+            .val;
     }
 
     fn add_at_head(&mut self, val: i32) {
-        let mut node = Box::new(Node {
+        let node = Box::new(Node {
             next: self.head.take(),
             val: val,
         });
@@ -39,7 +43,7 @@ impl MyLinkedList {
     }
 
     fn add_at_tail(&mut self, val: i32) {
-        let mut node = Box::new(Node {
+        let node = Box::new(Node {
             next: None,
             val: val,
         });
@@ -68,14 +72,14 @@ impl MyLinkedList {
                 head = &head.as_ref().unwrap().next;
                 i += 1;
             }
-            node.next = head.as_ref().unwrap().next.take();
-            head.as_mut().unwrap().next = Some(node);
+            node.next = head.as_ref().unwrap().clone().next.take();
+            self.head.as_mut().unwrap().next = Some(node);
         }
     }
 
     fn delete_at_index(&mut self, index: i32) {
         if index == 0 {
-            self.head = self.head.as_ref().unwrap().next.take();
+            self.head = self.head.as_ref().unwrap().clone().next.take();
         } else {
             let mut head = &self.head;
             let mut i = 0;
@@ -83,7 +87,9 @@ impl MyLinkedList {
                 head = &head.as_ref().unwrap().next;
                 i += 1;
             }
-            head.as_mut().unwrap().next = head.as_ref().unwrap().next.as_mut().unwrap().next.take();
+            if let Some(ref mut next) = head.as_ref().unwrap().clone().next {
+                self.head.as_mut().unwrap().next = next.next.take();
+            }
         }
     }
 }
@@ -98,11 +104,29 @@ impl MyLinkedList {
  * obj.delete_at_index(index);
  */
 fn main() {
+    // let mut obj = MyLinkedList::new();
+    // obj.add_at_head(1);
+    // obj.add_at_tail(3);
+    // obj.add_at_index(1, 2);
+    // println!("debug {:?}", obj);
+    // assert_eq!(obj.get(1), 2);
+    // obj.delete_at_index(1);
+    // println!("debug {:?}", obj);
+    // assert_eq!(obj.get(1), 3);
+
     let mut obj = MyLinkedList::new();
+    obj.add_at_head(7);
+    obj.add_at_head(2);
     obj.add_at_head(1);
-    obj.add_at_tail(3);
-    obj.add_at_index(1, 2);
-    assert_eq!(obj.get(1), 2);
-    obj.delete_at_index(1);
-    assert_eq!(obj.get(1), 3);
+    obj.add_at_index(3, 0);
+    println!("debug {:?}", obj);
+    obj.delete_at_index(2);
+    obj.add_at_head(6);
+    obj.add_at_tail(4);
+    println!("debug {:?}", obj);
+    assert_eq!(obj.get(4), 4);
+    obj.add_at_head(4);
+    obj.add_at_index(5, 0);
+    obj.add_at_head(6);
+    println!("debug {:?}", obj);
 }
