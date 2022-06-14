cf04690c6158b1ae6356da8fad0eb283e837ad35
akaishi <galigalikun@gmail.com>
2022-06-14 20:18:59 +0900

    wip number-of-atoms

--- problems/number-of-atoms/src/main.rs
+++ problems/number-of-atoms/src/main.rs
@@ -4,8 +4,39 @@ fn main() {
 }
 
 struct Solution{}
+use std::collections::HashMap;
 impl Solution {
     pub fn count_of_atoms(formula: String) -> String {
-        return formula;
+        let mut map = HashMap::new();
+        let mut stack = Vec::new();
+        let mut i = 0;
+        while i < formula.len() {
+            if formula.chars().nth(i) == Some('(') {
+                stack.push(i);
+            } else if formula.chars().nth(i) == Some(')') {
+                let start = stack.pop().unwrap();
+                let mut atom = String::new();
+                for j in start + 1..i {
+                    atom.push(formula.chars().nth(j).unwrap());
+                }
+                let mut count = 1;
+                while i + 1 < formula.len() && formula.chars().nth(i + 1) >= Some('0') && formula.chars().nth(i + 1) <= Some('9') {
+                    count *= formula.chars().nth(i + 1).unwrap() as u32 - '0' as u32;
+                    i += 1;
+                }
+                if let Some(v) = map.get_mut(&atom) {
+                    *v += count;
+                } else {
+                    map.insert(atom, count);
+                }
+            }
+            i += 1;
+        }
+        println!("{:?}", map);
+        let mut res = String::new();
+        for (k, v) in map.iter() {
+            res.push_str(&format!("{}{}", v, k));
+        }
+        return res;
     }
 }
