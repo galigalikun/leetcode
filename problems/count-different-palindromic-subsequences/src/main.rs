dc27e37e74b4a0a2636803e64a908c17cfbc2921
akaishi <galigalikun@gmail.com>
2022-06-18 07:47:57 +0900

    wip count-different-palindromic-subsequences

---
+++ problems/count-different-palindromic-subsequences/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    assert_eq!(Solution::count_palindromic_subsequences(String::from("bccb")), 6);
+    assert_eq!(Solution::count_palindromic_subsequences(String::from("abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba")), 104860361);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn count_palindromic_subsequences(s: String) -> i32 {
+        let mut dp: Vec<Vec<i128>> = vec![vec![0; s.len()]; s.len()];
+        for i in 0..s.len() {
+            dp[i][i] = 1;
+        }
+        for i in 0..s.len() {
+            for j in 0..i {
+                if s.chars().nth(i) == s.chars().nth(j) {
+                    if i - j < 2 {
+                        dp[j][i] = 2;
+                    } else {
+                        dp[j][i] = dp[j + 1][i - 1] * 2 + dp[j][i - 1];
+                    }
+                } else {
+                    dp[j][i] = dp[j][i - 1] + dp[j + 1][i];
+                }
+            }
+        }
+        return dp[0][s.len() - 1] as i32;
+    }
+}
