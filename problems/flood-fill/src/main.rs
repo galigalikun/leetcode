4da1dd3432715c651a8b059b958e6081866f0daf
akaishi <galigalikun@gmail.com>
2022-06-23 19:16:16 +0900

    add flood-fill

--- problems/flood-fill/src/main.rs
+++ problems/flood-fill/src/main.rs
@@ -14,24 +14,27 @@ impl Solution {
     pub fn flood_fill(image: Vec<Vec<i32>>, sr: i32, sc: i32, color: i32) -> Vec<Vec<i32>> {
         let mut image = image;
         let mut queue = Vec::new();
+        let idx = image[sr as usize][sc as usize];
         queue.push((sr, sc));
         while !queue.is_empty() {
             let (r, c) = queue.pop().unwrap();
             if image[r as usize][c as usize] == color {
                 continue;
             }
-            image[r as usize][c as usize] = color;
-            if r > 0 {
-                queue.push((r - 1, c));
-            }
-            if r < image.len() as i32 - 1 {
-                queue.push((r + 1, c));
-            }
-            if c > 0 {
-                queue.push((r, c - 1));
-            }
-            if c < image[0].len() as i32 - 1 {
-                queue.push((r, c + 1));
+            if image[r as usize][c as usize] == idx {
+                image[r as usize][c as usize] = color;
+                if r > 0 {
+                    queue.push((r - 1, c));
+                }
+                if r < image.len() as i32 - 1 {
+                    queue.push((r + 1, c));
+                }
+                if c > 0 {
+                    queue.push((r, c - 1));
+                }
+                if c < image[0].len() as i32 - 1 {
+                    queue.push((r, c + 1));
+                }
             }
         }
         return image;
