dc0fdbcc52c3cb3c6b8c0067610ecaa7d8672e5b
akaishi <galigalikun@gmail.com>
2022-06-25 12:22:43 +0900

    parse num

--- problems/parse-lisp-expression/src/main.rs
+++ problems/parse-lisp-expression/src/main.rs
@@ -13,6 +13,18 @@ fn main() {
 struct Solution {}
 impl Solution {
     pub fn evaluate(expression: String) -> i32 {
+        for c in expression.chars() {
+            match c {
+                '(' => {}
+                ')' => {}
+                ' ' => {}
+                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                    let num = c as i32 - 48;
+                    println!("debug {}", num);
+                }
+                _ => {}
+            }
+        }
         return 0;
     }
 }
