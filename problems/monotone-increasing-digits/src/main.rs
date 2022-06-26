32776d1ad42521122d9e7e4dc8194a178d8aefd1
akaishi <galigalikun@gmail.com>
2022-06-26 13:02:58 +0900

    wip monotone-increasing-digits

---
+++ problems/monotone-increasing-digits/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::monotone_increasing_digits(1), 9);
+    assert_eq!(Solution::monotone_increasing_digits(1234), 1234);
+    assert_eq!(Solution::monotone_increasing_digits(332), 299);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn monotone_increasing_digits(n: i32) -> i32 {
+        return 0;
+    }
+}
