8caab1e2af8ed45dd1e9597bbb2f8e82c29e294a
akaishi <galigalikun@gmail.com>
2022-07-12 19:50:11 +0900

    wip cracking-the-safe

---
+++ problems/cracking-the-safe/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::crack_safe(1, 2), "10");
+    assert_eq!(Solution::crack_safe(2, 2), "01100");
+}
+
+struct Solution{}
+impl Solution {
+    pub fn crack_safe(n: i32, k: i32) -> String {
+        return "".to_string();
+    }
+}
