f5c648c5275e65b0ec892806f9db46512a9a4a8a
akaishi <galigalikun@gmail.com>
2022-07-16 10:08:59 +0900

    wip special-binary-string

---
+++ problems/special-binary-string/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    assert_eq!(Solution::make_largest_special("11011000".to_string()), "11100100");
+    assert_eq!(Solution::make_largest_special("10".to_string()), "10");
+}
+struct Solution{}
+impl Solution {
+    pub fn make_largest_special(s: String) -> String {
+        return "".to_string();
+    }
+}
