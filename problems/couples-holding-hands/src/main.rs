cb00ec036ac8fa94383b7ef0a2675eda01554223
akaishi <galigalikun@gmail.com>
2022-07-21 17:36:01 +0900

    wip couples-holding-hands

---
+++ problems/couples-holding-hands/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    assert_eq!(Solution::min_swaps_couples(vec![0,2,1,3]), 1);
+    assert_eq!(Solution::min_swaps_couples(vec![3,2,0,1]), 0);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn min_swaps_couples(row: Vec<i32>) -> i32 {
+        for (i, v) in row.iter().enumerate() {
+
+        }
+        return 0;
+    }
+}
