63b7af5db8c27b73fc4fb9b28867424dd6164ec8
akaishi <galigalikun@gmail.com>
2022-07-24 19:58:40 +0900

    wip max-chunks-to-make-sorted

---
+++ problems/max-chunks-to-make-sorted/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::max_chunks_to_sorted(vec![4, 3, 2, 1, 0]), 1);
+    assert_eq!(Solution::max_chunks_to_sorted(vec![1, 0, 2, 3, 4]), 4);
+}
+
+struct Solution {}
+impl Solution {
+    pub fn max_chunks_to_sorted(arr: Vec<i32>) -> i32 {
+        return -1;
+    }
+}
