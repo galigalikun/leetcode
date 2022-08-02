7b674f2c8f268d544d252d72dda877d12c32e465
akaishi <galigalikun@gmail.com>
2022-08-02 17:43:44 +0900

    wip rabbits-in-forest

---
+++ problems/rabbits-in-forest/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::num_rabbits(vec![1, 1, 2]), 5);
+    assert_eq!(Solution::num_rabbits(vec![10, 10, 10]), 11);
+}
+
+struct Solution {}
+impl Solution {
+    pub fn num_rabbits(answers: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        return ans;
+    }
+}
