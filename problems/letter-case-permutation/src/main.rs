33d9680934971726204e99ae5f7c23a136e69380
akaishi <galigalikun@gmail.com>
2022-08-06 20:13:15 +0900

    debug

--- problems/letter-case-permutation/src/main.rs
+++ problems/letter-case-permutation/src/main.rs
@@ -1,3 +1,5 @@
+use std::ascii::AsciiExt;
+
 fn main() {
     assert_eq!(Solution::letter_case_permutation("a1b2".to_string()), vec!["a1b2","a1B2","A1b2","A1B2"]);
     assert_eq!(Solution::letter_case_permutation("3z4".to_string()), vec!["3z4","3Z4"]);
@@ -9,7 +11,7 @@ impl Solution {
         let mut ans = vec![1];
         for (i, c) in s.chars().enumerate() {
             if c.is_alphabetic() {
-                println!("debug {}", c);
+                println!("debug {}", c.to_ascii_uppercase());
             }
         }
         return vec![];
