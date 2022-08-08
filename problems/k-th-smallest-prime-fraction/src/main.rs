93abd48ce78b9dba1eb267768ca0ed120b0c2fb5
akaishi <galigalikun@gmail.com>
2022-08-08 20:42:49 +0900

    wip k-th-smallest-prime-fraction

---
+++ problems/k-th-smallest-prime-fraction/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::kth_smallest_prime_fraction(vec![1,2,3,5], 3), vec![2,5]);
+    assert_eq!(Solution::kth_smallest_prime_fraction(vec![1,7], 1), vec![1,7]);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn kth_smallest_prime_fraction(arr: Vec<i32>, k: i32) -> Vec<i32> {
+        return vec![];
+    }
+}
