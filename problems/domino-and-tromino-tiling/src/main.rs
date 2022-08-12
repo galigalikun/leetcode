f338ea4ef7942b8d7bed7e97e7da718e9a665c1a
akaishi <galigalikun@gmail.com>
2022-08-12 20:27:13 +0900

    wip domino-and-tromino-tiling

---
+++ problems/domino-and-tromino-tiling/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::num_tilings(3), 5);
+    assert_eq!(Solution::num_tilings(1), 1);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn num_tilings(n: i32) -> i32 {
+        return 0;
+    }
+}
