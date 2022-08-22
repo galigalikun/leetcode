67f528808d286e143ae8604c7077e82a0cbffa41
akaishi <galigalikun@gmail.com>
2022-08-23 00:57:46 +0900

    wip champagne-tower

---
+++ problems/champagne-tower/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::champagne_tower(1, 1, 1), 0.0);
+    assert_eq!(Solution::champagne_tower(2, 1, 1), 0.5);
+    assert_eq!(Solution::champagne_tower(100000009, 33, 17), 1.0);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn champagne_tower(poured: i32, query_row: i32, query_glass: i32) -> f64 {
+        return 0f64;
+    }
+}
