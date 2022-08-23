7dd24c620f35e375a14e2d7585304d9821ff3471
akaishi <galigalikun@gmail.com>
2022-08-24 00:48:11 +0900

    wip minimum-swaps-to-make-sequences-increasing

---
+++ problems/minimum-swaps-to-make-sequences-increasing/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    assert_eq!(Solution::min_swap(vec![1, 3, 5, 4], vec![1, 2, 3, 7]), 1);
+    assert_eq!(
+        Solution::min_swap(vec![0, 3, 5, 8, 9], vec![2, 1, 4, 6, 9]),
+        1
+    );
+}
+
+struct Solution {}
+impl Solution {
+    pub fn min_swap(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
