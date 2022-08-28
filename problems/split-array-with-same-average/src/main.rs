44d5ebcf530ca03cc362bc8b891c5e55488eddde
akaishi <galigalikun@gmail.com>
2022-08-28 13:37:11 +0900

    wip split-array-with-same-average

--- problems/split-array-with-same-average/src/main.rs
+++ problems/split-array-with-same-average/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Solution::split_array_same_average(vec![1,2,3,4,5,6,7,8]), true);
+    assert_eq!(Solution::split_array_same_average(vec![3,1]), false);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn split_array_same_average(nums: Vec<i32>) -> bool {
+        return false;
+    }
 }
