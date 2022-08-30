509dac57ee85c3af36f4900024a3e7750789a351
akaishi <galigalikun@gmail.com>
2022-08-30 22:31:58 +0900

    wip soup-servings

---
+++ problems/soup-servings/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::soup_servings(50), 0.62500);
+    assert_eq!(Solution::soup_servings(100), 0.71875);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn soup_servings(n: i32) -> f64 {
+        return 0f64;
+    }
+}
