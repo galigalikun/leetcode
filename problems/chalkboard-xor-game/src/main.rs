6ac74757768fe28a96809f6933269e7aa56758d4
akaishi <galigalikun@gmail.com>
2022-09-01 21:00:02 +0900

    wip chalkboard-xor-game

---
+++ problems/chalkboard-xor-game/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::xor_game(vec![1,1,2]), false);
+    assert_eq!(Solution::xor_game(vec![0,1]), true);
+    assert_eq!(Solution::xor_game(vec![1,2,3]), true);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn xor_game(nums: Vec<i32>) -> bool {
+        return false;
+    }
+}
