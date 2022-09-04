f9e96b16e94a0527218cd2c46f6bd5598cfb277c
akaishi <galigalikun@gmail.com>
2022-09-04 23:05:22 +0900

    wip largest-sum-of-averages

---
+++ problems/largest-sum-of-averages/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::largest_sum_of_averages(vec![9,1,2,3,9], 3), 20.0);
+    assert_eq!(Solution::largest_sum_of_averages(vec![1,2,3,4,5,6,7], 4), 20.5);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn largest_sum_of_averages(nums: Vec<i32>, k: i32) -> f64 {
+        return 0f64;
+    }
+}
