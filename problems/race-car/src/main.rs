1590692a9430186a873b309f5d892528f7a37252
akaishi <galigalikun@gmail.com>
2022-09-10 22:57:14 +0900

    wip race-car

---
+++ problems/race-car/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::racecar(3), 2);
+    assert_eq!(Solution::racecar(6), 5);
+}
+
+struct Solution {}
+impl Solution {
+    pub fn racecar(target: i32) -> i32 {
+        return -1;
+    }
+}
