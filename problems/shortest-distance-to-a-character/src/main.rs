33566617da021db3b57f6607d6994147e7fe8709
akaishi <galigalikun@gmail.com>
2022-09-15 20:46:09 +0900

    add shortest-distance-to-a-character

--- problems/shortest-distance-to-a-character/src/main.rs
+++ problems/shortest-distance-to-a-character/src/main.rs
@@ -7,18 +7,29 @@ fn main() {
         Solution::shortest_to_char("aaab".to_string(), 'b'),
         vec![3, 2, 1, 0]
     );
+    assert_eq!(
+        Solution::shortest_to_char("aaba".to_string(), 'b'),
+        vec![2, 1, 0, 1]
+    );
 }
 
 struct Solution {}
 impl Solution {
     pub fn shortest_to_char(s: String, c: char) -> Vec<i32> {
         let mut ans = vec![-1; s.len()];
-
+        let mut idx = None;
         for (i, p) in s.chars().enumerate() {
             if p == c {
                 ans[i] = 0;
+                idx = Some(i);
             } else if let Some(p) = s[i..].chars().position(|v| v == c) {
-                ans[i] = p as i32;
+                if let Some(id) = idx {
+                    ans[i] = std::cmp::min(i - id, p) as i32;
+                } else {
+                    ans[i] = p as i32;
+                }
+            } else if let Some(id) = idx {
+                ans[i] = (i - id) as i32;
             }
         }
         return ans;
