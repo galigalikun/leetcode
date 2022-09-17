ee1b8546c9a7f76354caea6bb821e5b6f45949a0
akaishi <galigalikun@gmail.com>
2022-09-17 11:46:12 +0900

    wip binary-trees-with-factors

---
+++ problems/binary-trees-with-factors/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::num_factored_binary_trees(vec![2,4]), 3);
+    assert_eq!(Solution::num_factored_binary_trees(vec![2,4,5,10]), 7);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn num_factored_binary_trees(arr: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
