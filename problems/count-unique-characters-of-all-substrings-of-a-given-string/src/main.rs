96647b4e40d4459a2e5ad8d30b5d5d5f5a96c1b4
akaishi <galigalikun@gmail.com>
2022-09-23 01:57:51 +0900

    wip count-unique-characters-of-all-substrings-of-a-given-string

---
+++ problems/count-unique-characters-of-all-substrings-of-a-given-string/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::unique_letter_string("ABC".to_string()), 10);
+    assert_eq!(Solution::unique_letter_string("ABA".to_string()), 8);
+    assert_eq!(Solution::unique_letter_string("LEETCODE".to_string()), 92);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn unique_letter_string(s: String) -> i32 {
+        return 0;
+    }
+}
