fc8b8021675159f8463801ba82b169ba2b802c4b
akaishi <galigalikun@gmail.com>
2022-09-23 23:25:40 +0900

    wip consecutive-numbers-sum

---
+++ problems/consecutive-numbers-sum/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::consecutive_numbers_sum(5), 2);
+    assert_eq!(Solution::consecutive_numbers_sum(9), 3);
+    assert_eq!(Solution::consecutive_numbers_sum(15), 4);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn consecutive_numbers_sum(n: i32) -> i32 {
+        return 0;
+    }
+}
