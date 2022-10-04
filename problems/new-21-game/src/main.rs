15c476ea80e9976dcd1e29bac2579664a9fd9da1
akaishi <galigalikun@gmail.com>
2022-10-04 20:28:36 +0900

    wip new-21-game

---
+++ problems/new-21-game/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::new21_game(10, 1, 10), 1.0);
+    assert_eq!(Solution::new21_game(6, 1, 10), 0.6);
+    assert_eq!(Solution::new21_game(21, 17, 10), 0.73278);
+}
+
+struct Solution{}
+impl Solution {
+    pub fn new21_game(n: i32, k: i32, max_pts: i32) -> f64 {
+        return 0.0;
+    }
+}
