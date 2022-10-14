8d9fc11534f3c5a5bd5632a738f623e018f2c53f
akaishi <galigalikun@gmail.com>
2022-10-14 22:39:35 +0900

    wip hand-of-straights

---
+++ problems/hand-of-straights/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::is_n_straight_hand(vec![1,2,3,6,2,3,4,7,8], 3), true);
+    assert_eq!(Solution::is_n_straight_hand(vec![1,2,3,4,5], 4), false);
+}
+
+struct Solution;
+impl Solution {
+    pub fn is_n_straight_hand(hand: Vec<i32>, group_size: i32) -> bool {
+        return false;
+    }
+}
