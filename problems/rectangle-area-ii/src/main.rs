84637f3dc5e42cee26d7228a8fb50101fc791c89
akaishi <galigalikun@gmail.com>
2022-10-19 00:27:09 +0900

    wip rectangle-area-ii

---
+++ problems/rectangle-area-ii/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::rectangle_area(vec![vec![0,0,2,2],vec![1,0,2,3],vec![1,0,3,1]]), 6);
+    assert_eq!(Solution::rectangle_area(vec![vec![0,0,1000000000,1000000000]]), 49);
+}
+
+struct Solution;
+impl Solution {
+    pub fn rectangle_area(rectangles: Vec<Vec<i32>>) -> i32 {
+        return 0;
+    }
+}
