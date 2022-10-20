6b3bd2ac9ced7fd143aceadf3591af3241a71844
akaishi <galigalikun@gmail.com>
2022-10-21 01:55:00 +0900

    wip peak-index-in-a-mountain-array

---
+++ problems/peak-index-in-a-mountain-array/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::peak_index_in_mountain_array(vec![0,1,0]), 1);
+    assert_eq!(Solution::peak_index_in_mountain_array(vec![0,2,1,0]), 1);
+    assert_eq!(Solution::peak_index_in_mountain_array(vec![0,10,5,2]), 1);
+}
+
+struct Solution;
+impl Solution {
+    pub fn peak_index_in_mountain_array(arr: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
