b353b1a687c4610d7cf9616ab658e67e0ae72c12
akaishi <galigalikun@gmail.com>
2022-10-28 01:13:57 +0900

    wip mirror-reflection

---
+++ problems/mirror-reflection/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::mirror_reflection(2, 1), 2);
+    assert_eq!(Solution::mirror_reflection(3, 1), 1);
+}
+
+struct Solution;
+impl Solution {
+    pub fn mirror_reflection(p: i32, q: i32) -> i32 {
+        return 0;
+    }
+}
