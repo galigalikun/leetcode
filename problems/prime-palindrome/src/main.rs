06969a7ff698437403506c518b10e2f89d800416
akaishi <galigalikun@gmail.com>
2022-11-07 23:20:50 +0900

    wip prime-palindrome

---
+++ problems/prime-palindrome/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::prime_palindrome(6), 7);
+    assert_eq!(Solution::prime_palindrome(8), 11);
+    assert_eq!(Solution::prime_palindrome(13), 101);
+}
+
+struct Solution;
+impl Solution {
+    pub fn prime_palindrome(n: i32) -> i32 {
+        return 0;
+    }
+}
