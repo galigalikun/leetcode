68f86419962a6ecb0246735e17238b398bcbff50
akaishi <galigalikun@gmail.com>
2022-11-11 23:19:09 +0900

    wip reordered-power-of-2

---
+++ problems/reordered-power-of-2/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::reordered_power_of2(1), true);
+    assert_eq!(Solution::reordered_power_of2(10), false);
+}
+
+struct Solution;
+impl Solution {
+    pub fn reordered_power_of2(n: i32) -> bool {
+        return false;
+    }
+}
