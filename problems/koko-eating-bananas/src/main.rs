1b27dd0182e6e93caa8976227f8ef53ca670a899
akaishi <galigalikun@gmail.com>
2022-11-19 07:22:53 +0900

    wip koko-eating-bananas

---
+++ problems/koko-eating-bananas/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::min_eating_speed(vec![3,6,7,11], 8), 4);
+    assert_eq!(Solution::min_eating_speed(vec![30,11,23,4,20], 5), 30);
+    assert_eq!(Solution::min_eating_speed(vec![30,11,23,4,20], 6), 23);
+}
+
+struct Solution;
+impl Solution {
+    pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+        return 0;
+    }
+}
