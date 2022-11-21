d1c4d8b4213b2aeac3e25139e793ce8fb9704427
akaishi <galigalikun@gmail.com>
2022-11-21 21:46:19 +0900

    add stone-game

---
+++ problems/stone-game/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::stone_game(vec![5,3,4,5]), true);
+    assert_eq!(Solution::stone_game(vec![3,7,2,3]), true);
+}
+
+struct Solution;
+impl Solution {
+    pub fn stone_game(piles: Vec<i32>) -> bool {
+        return true;
+    }
+}
