28dce3ca11af57fda548bc9f99f8fdaf3d8a8e86
akaishi <galigalikun@gmail.com>
2022-11-22 21:32:39 +0900

    wip nth-magical-number

---
+++ problems/nth-magical-number/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::nth_magical_number(1, 2, 3), 2);
+    assert_eq!(Solution::nth_magical_number(4, 2, 3), 6);
+}
+
+struct Solution;
+impl Solution {
+    pub fn nth_magical_number(n: i32, a: i32, b: i32) -> i32 {
+        return 0;
+    }
+}
