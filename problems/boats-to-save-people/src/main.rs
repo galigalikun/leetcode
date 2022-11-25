9d392000f92a8a7fdddee95600ef08072baf3ec7
akaishi <galigalikun@gmail.com>
2022-11-25 22:38:09 +0900

    wip boats-to-save-people

---
+++ problems/boats-to-save-people/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::num_rescue_boats(vec![1,2], 3), 1);
+    assert_eq!(Solution::num_rescue_boats(vec![3,2,2,1], 3), 3);
+    assert_eq!(Solution::num_rescue_boats(vec![3,5,3,4], 5), 4);
+}
+
+struct Solution;
+impl Solution {
+    pub fn num_rescue_boats(people: Vec<i32>, limit: i32) -> i32 {
+        return 0;
+    }
+}
