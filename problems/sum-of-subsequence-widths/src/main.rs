ff0bdd9826f48f144d0183acd57602abf4e98295
akaishi <galigalikun@gmail.com>
2022-12-07 01:40:05 +0900

    wip sum-of-subsequence-widths

---
+++ problems/sum-of-subsequence-widths/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::sum_subseq_widths(vec![2,1,3]), 6);
+    assert_eq!(Solution::sum_subseq_widths(vec![2]), 0);
+}
+
+struct Solution;
+impl Solution {
+    pub fn sum_subseq_widths(nums: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
