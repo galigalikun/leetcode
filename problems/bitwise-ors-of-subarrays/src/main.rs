8ef7c4647150400c7630302052f585aa0a344fb3
akaishi <galigalikun@gmail.com>
2022-12-14 20:23:04 +0900

    wip bitwise-ors-of-subarrays

---
+++ problems/bitwise-ors-of-subarrays/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::subarray_bitwise_o_rs(vec![0]), 1);
+    assert_eq!(Solution::subarray_bitwise_o_rs(vec![1,1,2]), 3);
+}
+
+struct Solution;
+impl Solution {
+    pub fn subarray_bitwise_o_rs(arr: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
