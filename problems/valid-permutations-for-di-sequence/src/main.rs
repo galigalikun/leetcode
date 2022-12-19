b896e59d7564a5b9b9f6a9a65a1ae4e535e83f02
akaishi <galigalikun@gmail.com>
2022-12-19 21:38:12 +0900

    wip valid-permutations-for-di-sequence

---
+++ problems/valid-permutations-for-di-sequence/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::num_perms_di_sequence("DID".to_string()), 5);
+    assert_eq!(Solution::num_perms_di_sequence("D".to_string()), 1);
+}
+
+struct Solution;
+impl Solution {
+    pub fn num_perms_di_sequence(s: String) -> i32 {
+        return -1;
+    }
+}
