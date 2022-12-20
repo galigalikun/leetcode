0bb14f5c51c35d1d37bb205aa1961af6afd36fae
akaishi <galigalikun@gmail.com>
2022-12-20 23:40:09 +0900

    wip fruit-into-baskets

---
+++ problems/fruit-into-baskets/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::total_fruit(vec![1,2,1]), 3);
+    assert_eq!(Solution::total_fruit(vec![0,1,2,2]), 3);
+    assert_eq!(Solution::total_fruit(vec![1,2,3,2,2]), 4);
+}
+
+struct Solution;
+impl Solution {
+    pub fn total_fruit(fruits: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
