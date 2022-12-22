2554a8dfed45530dba4d19be3618fb820e6cb6df
akaishi <galigalikun@gmail.com>
2022-12-22 20:48:38 +0900

    wip super-palindromes

---
+++ problems/super-palindromes/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::superpalindromes_in_range("4".to_string(), "1000".to_string()), 0);
+    assert_eq!(Solution::superpalindromes_in_range("1".to_string(), "2".to_string()), 1);
+}
+
+struct Solution;
+impl Solution {
+    pub fn superpalindromes_in_range(left: String, right: String) -> i32 {
+        return 0;
+    }
+}
