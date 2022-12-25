6eb6f15473d377b2c19a2aed166cfd675bef1595
akaishi <galigalikun@gmail.com>
2022-12-26 01:02:19 +0900

    wip sum-of-subarray-minimums

---
+++ problems/sum-of-subarray-minimums/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::sum_subarray_mins(vec![3,1,2,4]), 17);
+    assert_eq!(Solution::sum_subarray_mins(vec![11,81,94,43,3]), 444);
+}
+
+struct Solution;
+impl Solution {
+    pub fn sum_subarray_mins(arr: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
