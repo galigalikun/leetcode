ebbec714f6847df2b585babbd4014228d04f1342
akaishi <galigalikun@gmail.com>
2022-12-28 21:33:49 +0900

    wip smallest-range-ii

---
+++ problems/smallest-range-ii/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::smallest_range_ii(vec![1], 0), 0);
+    assert_eq!(Solution::smallest_range_ii(vec![0,10], 2), 6);
+    assert_eq!(Solution::smallest_range_ii(vec![1,3,6], 3), 3);
+}
+
+struct Solution;
+impl Solution {
+    pub fn smallest_range_ii(nums: Vec<i32>, k: i32) -> i32 {
+        return 0;
+    }
+}
