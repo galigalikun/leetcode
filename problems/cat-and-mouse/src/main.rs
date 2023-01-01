b07bb67d664cff603b1c726bbbb3dd5f65412f14
akaishi <galigalikun@gmail.com>
2023-01-01 18:39:27 +0900

    wip cat-and-mouse

---
+++ problems/cat-and-mouse/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::cat_mouse_game(vec![vec![2,5],vec![3],vec![0,4,5],vec![1,4,5],vec![2,3],vec![0,2,3]]), 2);
+    assert_eq!(Solution::cat_mouse_game(vec![vec![1,3],vec![0],vec![3],vec![0,2]]), 1);
+}
+
+struct Solution;
+impl Solution {
+    pub fn cat_mouse_game(graph: Vec<Vec<i32>>) -> i32 {
+        return 0;
+    }
+}
