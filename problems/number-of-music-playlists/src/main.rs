3f1e996fd4e9e627e11e1f067994ba7e13d2a6db
akaishi <galigalikun@gmail.com>
2023-01-11 20:52:02 +0900

    wip number-of-music-playlists

---
+++ problems/number-of-music-playlists/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::num_music_playlists(3, 3, 1), 6);
+    assert_eq!(Solution::num_music_playlists(2, 3, 0), 6);
+    assert_eq!(Solution::num_music_playlists(2, 3, 1), 2);
+}
+
+struct Solution;
+impl Solution {
+    pub fn num_music_playlists(n: i32, goal: i32, k: i32) -> i32 {
+        return 0;
+    }
+}
