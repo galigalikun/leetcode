97dc10f614faefceae4f282bc3bc38815fabaff4
akaishi <galigalikun@gmail.com>
2023-01-17 23:31:01 +0900

    wip long-pressed-name

---
+++ problems/long-pressed-name/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::is_long_pressed_name("alex".to_string(), "aaleex".to_string()), true);
+    assert_eq!(Solution::is_long_pressed_name("saeed".to_string(), "ssaaedd".to_string()), false);
+}
+
+struct Solution;
+impl Solution {
+    pub fn is_long_pressed_name(name: String, typed: String) -> bool {
+        return false;
+    }
+}
