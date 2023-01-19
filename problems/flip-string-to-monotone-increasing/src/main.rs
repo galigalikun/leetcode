d3a6b03873872ad0ae0849032a55cc84bdcf34a1
akaishi <galigalikun@gmail.com>
2023-01-19 20:27:12 +0900

    wip flip-string-to-monotone-increasing

---
+++ problems/flip-string-to-monotone-increasing/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::min_flips_mono_incr("00110".to_string()), 1);
+    assert_eq!(Solution::min_flips_mono_incr("010110".to_string()), 2);
+    assert_eq!(Solution::min_flips_mono_incr("00011000".to_string()), 2);
+}
+
+struct Solution;
+impl Solution {
+    pub fn min_flips_mono_incr(s: String) -> i32 {
+        return 0;
+    }
+}
