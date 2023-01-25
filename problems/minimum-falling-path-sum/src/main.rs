004823343f8d9e4100c60dc7c634be36485e6ad1
akaishi <galigalikun@gmail.com>
2023-01-26 02:23:50 +0900

    wip minimum-falling-path-sum

---
+++ problems/minimum-falling-path-sum/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::min_falling_path_sum(vec![vec![2,1,3],vec![6,5,4],vec![7,8,9]]), 13);
+    assert_eq!(Solution::min_falling_path_sum(vec![vec![-19,57],vec![-40,-5]]), -59);
+}
+
+struct Solution;
+impl Solution {
+    pub fn min_falling_path_sum(matrix: Vec<Vec<i32>>) -> i32 {
+        return 0;
+    }
+}
