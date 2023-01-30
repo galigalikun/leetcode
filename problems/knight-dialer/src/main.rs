a79451735adb1ef3ca736d4c8babfa3d59b0e6b9
akaishi <galigalikun@gmail.com>
2023-01-31 00:01:27 +0900

    wip knight-dialer

---
+++ problems/knight-dialer/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::knight_dialer(1), 10);
+    assert_eq!(Solution::knight_dialer(2), 20);
+    assert_eq!(Solution::knight_dialer(3131), 136006598);
+}
+
+struct Solution;
+impl Solution {
+    pub fn knight_dialer(n: i32) -> i32 {
+        return 0;
+    }
+}
