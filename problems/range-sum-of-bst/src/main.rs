5a602b1a58e59e8178134464bc6a454ba9116fb9
akaishi <galigalikun@gmail.com>
2023-02-02 21:04:59 +0900

    wip range-sum-of-bst

---
+++ problems/range-sum-of-bst/src/main.rs
@@ -0,0 +1,105 @@
+fn main() {
+    assert_eq!(
+        Solution::range_sum_bst(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 10,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 3,
+                        left: None,
+                        right: None,
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 7,
+                        left: None,
+                        right: None,
+                    }))),
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 15,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 18,
+                        left: None,
+                        right: None,
+                    }))),
+                })))
+            }))),
+            7,
+            15
+        ),
+        32
+    );
+    assert_eq!(
+        Solution::range_sum_bst(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 10,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 3,
+                        left: Some(Rc::new(RefCell::new(TreeNode {
+                            va1,
+                            left: None,
+                            right: None,
+                        }))),
+                        right: None,
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 7,
+                        left: Some(Rc::new(RefCell::new(TreeNode {
+                            val: 6,
+                            left: None,
+                            right: None,
+                        }))),
+                        right: None,
+                    }))),
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 15,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 13,
+                        left: None,
+                        right: None,
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 18,
+                        left: None,
+                        right: None,
+                    }))),
+                }))),
+            }))),
+            6,
+            10
+        ),
+        23
+    );
+}
+
+struct Solution;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+        return 0;
+    }
+}
