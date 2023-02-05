f86c425aac3622647b9e6eb11d66e03f17011e85
akaishi <galigalikun@gmail.com>
2023-02-06 01:33:30 +0900

    wip distinct-subsequences-ii

---
+++ problems/distinct-subsequences-ii/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::distinct_subseq_ii("abc".to_string()), 7);
+    assert_eq!(Solution::distinct_subseq_ii("aba".to_string()), 6);
+    assert_eq!(Solution::distinct_subseq_ii("aaa".to_string()), 3);
+}
+
+struct Solution;
+impl Solution {
+    pub fn distinct_subseq_ii(s: String) -> i32 {
+        return 0;
+    }
+}
