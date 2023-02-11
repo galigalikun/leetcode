54aa2943a2269fd8feae85ab7c4cf36049bc5824
akaishi <galigalikun@gmail.com>
2023-02-11 10:02:13 +0900

    add delete-columns-to-make-sorted

--- problems/delete-columns-to-make-sorted/src/main.rs
+++ problems/delete-columns-to-make-sorted/src/main.rs
@@ -25,14 +25,18 @@ struct Solution;
 impl Solution {
     pub fn min_deletion_size(strs: Vec<String>) -> i32 {
         let mut ans = 0;
-        for (i, s) in strs.into_iter().enumerate() {
+        let mut v = vec!["".to_string(); strs[0].len()];
+        for (_i, s) in strs.into_iter().enumerate() {
+            for (j, c) in s.chars().enumerate() {
+                v[j] = format!("{}{}", v[j], c);
+            }
+        }
+
+        for s in v {
             let mut a = s.chars().collect::<Vec<_>>();
             a.sort();
-            let b = a.iter().collect::<String>();
-            let c = a.iter().rev().collect::<String>();
-            if s != b && s != c {
-                ans = i as i32;
-                break;
+            if s != a.iter().collect::<String>() {
+                ans += 1;
             }
         }
         return ans;
