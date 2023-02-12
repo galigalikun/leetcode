a401961f8dc11f25544bb4d7f68d1d7568cd58d2
akaishi <galigalikun@gmail.com>
2023-02-13 02:26:09 +0900

    wip minimum-increment-to-make-array-unique

---
+++ problems/minimum-increment-to-make-array-unique/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::min_increment_for_unique(vec![1,2,2]), 1);
+    assert_eq!(Solution::min_increment_for_unique(vec![3,2,1,2,1,7]), 6);
+}
+
+struct Solution;
+impl Solution {
+    pub fn min_increment_for_unique(nums: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
