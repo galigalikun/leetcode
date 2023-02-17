c818f0d98e134ba7dfbbec0082ccd705c3ac0721
akaishi <galigalikun@gmail.com>
2023-02-18 00:41:54 +0900

    wip largest-time-for-given-digits

---
+++ problems/largest-time-for-given-digits/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::largest_time_from_digits(vec![1,2,3,4]), "23:41");
+    assert_eq!(Solution::largest_time_from_digits(vec![5,5,5,5]), "");
+}
+
+struct Solution;
+impl Solution {
+    pub fn largest_time_from_digits(arr: Vec<i32>) -> String {
+        return "".to_string();
+    }
+}
