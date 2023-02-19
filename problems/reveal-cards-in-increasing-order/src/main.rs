f5d2e6ce287b72d328026568f3e6adf92b289e28
akaishi <galigalikun@gmail.com>
2023-02-20 00:01:02 +0900

    wip reveal-cards-in-increasing-order

---
+++ problems/reveal-cards-in-increasing-order/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::deck_revealed_increasing(vec![17,13,11,2,3,5,7]), vec![2,13,3,11,5,17,7]);
+    assert_eq!(Solution::deck_revealed_increasing(vec![1,1000]), vec![1,1000]);
+}
+
+struct Solution;
+impl Solution {
+    pub fn deck_revealed_increasing(deck: Vec<i32>) -> Vec<i32> {
+        return vec![];
+    }
+}
