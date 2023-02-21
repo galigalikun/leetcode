547e189c41afc46269e50c01bfe6e2460c47d91d
akaishi <galigalikun@gmail.com>
2023-02-21 21:03:00 +0900

    wip largest-component-size-by-common-factor

---
+++ problems/largest-component-size-by-common-factor/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::largest_component_size(vec![4,6,15,35]), 4);
+    assert_eq!(Solution::largest_component_size(vec![20,50,9,63]), 2);
+    assert_eq!(Solution::largest_component_size(vec![2,3,6,7,4,12,21,39]), 8);
+}
+
+struct Solution;
+impl Solution {
+    pub fn largest_component_size(nums: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
