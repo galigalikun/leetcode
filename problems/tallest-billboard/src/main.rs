debb2bfe8ce0d77af8a545d685e07c8afdfb2c62
akaishi <galigalikun@gmail.com>
2023-02-27 02:00:11 +0900

    wip tallest-billboard

---
+++ problems/tallest-billboard/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::tallest_billboard(vec![1,2,3,6]), 6);
+    assert_eq!(Solution::tallest_billboard(vec![1,2,3,4,5,6]), 10);
+    assert_eq!(Solution::tallest_billboard(vec![1,2]), 0);
+}
+
+struct Solution;
+impl Solution {
+    pub fn tallest_billboard(rods: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
