c05114900c68450a608385ef6648999f2878fdbf
akaishi <galigalikun@gmail.com>
2023-02-28 22:21:29 +0900

    wip check-completeness-of-a-binary-tree

---
+++ problems/check-completeness-of-a-binary-tree/src/main.rs
@@ -0,0 +1,90 @@
+fn main() {
+    assert_eq!(
+        Solution::is_complete_tree(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: None
+                }))),
+                right: None
+            }))),
+        })))),
+        true
+    );
+
+    assert_eq!(
+        Solution::is_complete_tree(Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 7,
+                    left: None,
+                    right: None
+                })))
+            }))),
+        })))),
+        false
+    );
+}
+
+struct Solution;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn is_complete_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        if let Some(r) = root {
+            Self::is_complete_tree(r.borrow().left.clone());
+            Self::is_complete_tree(r.borrow().right.clone());
+        }
+        return false;
+    }
+}
