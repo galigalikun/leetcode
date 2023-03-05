c65561e027e6c9517ca96b4f8dac426590480141
akaishi <galigalikun@gmail.com>
2023-03-06 01:07:27 +0900

    wip maximum-width-ramp

---
+++ problems/maximum-width-ramp/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::max_width_ramp(vec![6,0,8,2,1,5]), 4);
+    assert_eq!(Solution::max_width_ramp(vec![9,8,1,0,1,9,4,0,4,1]), 7);
+}
+
+struct Solution;
+impl Solution {
+    pub fn max_width_ramp(nums: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
