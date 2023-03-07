bc55b6525d2f706d8b4ba1d5c246b5d4fc21c717
akaishi <galigalikun@gmail.com>
2023-03-08 01:24:55 +0900

    wip least-operators-to-express-number

---
+++ problems/least-operators-to-express-number/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::least_ops_express_target(3, 19), 5);
+    assert_eq!(Solution::least_ops_express_target(5, 501), 8);
+    assert_eq!(Solution::least_ops_express_target(100, 100000000), 3);
+}
+
+struct Solution;
+impl Solution {
+    pub fn least_ops_express_target(x: i32, target: i32) -> i32 {
+        return 0;
+    }
+}
