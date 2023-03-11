5d089ac77a9a041356c367b2dacacc6b7478efc4
akaishi <galigalikun@gmail.com>
2023-03-12 01:30:44 +0900

    wip numbers-with-same-consecutive-differences

---
+++ problems/numbers-with-same-consecutive-differences/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::nums_same_consec_diff(3, 7), vec![181,292,707,818,929]);
+    assert_eq!(Solution::nums_same_consec_diff(2, 1), vec![10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]);
+}
+
+struct Solution;
+impl Solution {
+    pub fn nums_same_consec_diff(n: i32, k: i32) -> Vec<i32> {
+        return vec![];
+    }
+}
