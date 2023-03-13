5cc4f8b26b3b8216f89b6473045b8d1b24b2b525
akaishi <galigalikun@gmail.com>
2023-03-13 23:14:57 +0900

    wip pancake-sorting

---
+++ problems/pancake-sorting/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::pancake_sort(vec![3,2,4,1]), vec![4,2,4,3]);
+    assert_eq!(Solution::pancake_sort(vec![1,2,3]), vec![]);
+}
+
+struct Solution;
+impl Solution {
+    pub fn pancake_sort(arr: Vec<i32>) -> Vec<i32> {
+        return vec![];
+    }
+}
