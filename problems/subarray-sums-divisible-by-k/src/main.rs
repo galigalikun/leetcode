56a3c03a8d4a4b9564b083e65f09761fd0fbd676
akaishi <galigalikun@gmail.com>
2023-03-18 23:39:56 +0900

    wip subarray-sums-divisible-by-k

---
+++ problems/subarray-sums-divisible-by-k/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::subarrays_div_by_k(vec![4,5,0,-2,-3,1], 5), 7);
+    assert_eq!(Solution::subarrays_div_by_k(vec![5], 9), 0);
+}
+
+struct Solution;
+impl Solution {
+    pub fn subarrays_div_by_k(nums: Vec<i32>, k: i32) -> i32 {
+        return 0;
+    }
+}
