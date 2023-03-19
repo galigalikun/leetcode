557fc307464d3aa3fbff4df448cf8a23e81a92aa
akaishi <galigalikun@gmail.com>
2023-03-20 01:13:42 +0900

    wip odd-even-jump

---
+++ problems/odd-even-jump/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::odd_even_jumps(vec![10,13,12,14,15]), 2);
+    assert_eq!(Solution::odd_even_jumps(vec![2,3,1,1,4]), 3);
+    assert_eq!(Solution::odd_even_jumps(vec![5,1,3,4,2]), 3);
+}
+
+struct Solution;
+impl Solution {
+    pub fn odd_even_jumps(arr: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
