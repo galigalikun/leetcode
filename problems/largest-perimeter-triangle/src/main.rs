90d3bc2c177e8f00867d33286dbfc42e4a9c678f
akaishi <galigalikun@gmail.com>
2023-03-20 22:51:15 +0900

    wip largest-perimeter-triangle

---
+++ problems/largest-perimeter-triangle/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::largest_perimeter(vec![2,1,2]), 5);
+    assert_eq!(Solution::largest_perimeter(vec![1,2,1,10]), 10);
+}
+
+struct Solution;
+impl Solution {
+    pub fn largest_perimeter(nums: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
