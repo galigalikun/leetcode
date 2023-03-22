6157ff73ec59ccb04b8483f69cc505decb087346
akaishi <galigalikun@gmail.com>
2023-03-23 01:12:58 +0900

    wip longest-turbulent-subarray

---
+++ problems/longest-turbulent-subarray/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    assert_eq!(Solution::max_turbulence_size(vec![9,4,2,10,7,8,8,1,9]), 5);
+    assert_eq!(Solution::max_turbulence_size(vec![4,8,12,16]), 2);
+    assert_eq!(Solution::max_turbulence_size(vec![100]), 1);
+}
+
+struct Solution;
+impl Solution {
+    pub fn max_turbulence_size(arr: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
