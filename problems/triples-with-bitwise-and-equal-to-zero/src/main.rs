cb3aff2c940f734eff555f5690ce08fdda9aec66
akaishi <galigalikun@gmail.com>
2023-03-28 23:46:43 +0900

    wip triples-with-bitwise-and-equal-to-zero

---
+++ problems/triples-with-bitwise-and-equal-to-zero/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::count_triplets(vec![2,1,3]), 12);
+    assert_eq!(Solution::count_triplets(vec![0,0,0]), 27);
+}
+
+struct Solution;
+impl Solution {
+    pub fn count_triplets(nums: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
