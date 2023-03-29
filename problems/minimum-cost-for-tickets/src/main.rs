7bfaca142dc1a92f0302cf282922a124054bb212
akaishi <galigalikun@gmail.com>
2023-03-29 21:59:04 +0900

    wip minimum-cost-for-tickets

---
+++ problems/minimum-cost-for-tickets/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::mincost_tickets(vec![1,4,6,7,8,20], vec![2,7,15]), 11);
+    assert_eq!(Solution::mincost_tickets(vec![1,2,3,4,5,6,7,8,9,10,30,31], vec![2,7,15]), 17);
+}
+
+struct Solution;
+impl Solution {
+    pub fn mincost_tickets(days: Vec<i32>, costs: Vec<i32>) -> i32 {
+        return 0;
+    }
+}
