4653a947e123b9354b99ac9adac5d4bd302ff05b
akaishi <galigalikun@gmail.com>
2023-04-25 07:59:26 +0900

    wip clumsy-factorial

--- problems/clumsy-factorial/src/main.rs
+++ problems/clumsy-factorial/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Solution::clumsy(4), 7);
+    assert_eq!(Solution::clumsy(10), 12);
+}
+
+struct Solution;
+impl Solution {
+    pub fn clumsy(n: i32) -> i32 {
+        let mut result = 0;
+        let mut n = n;
+        let mut sign = 1;
+        while n > 0 {
+            let mut temp = n;
+            n -= 1;
+            if n > 0 {
+                temp *= n;
+                n -= 1;
+            }
+            if n > 0 {
+                temp /= n;
+                n -= 1;
+            }
+            if n > 0 {
+                temp += n;
+                n -= 1;
+            }
+            result += sign * temp;
+            sign = -1;
+        }
+        return result;
+    }
 }
