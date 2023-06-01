557548b5324a41fab6a52054b44f9f9e711b9ee8
akaishi <galigalikun@gmail.com>
2023-06-01 23:48:27 +0900

    wip binary-search-tree-to-greater-sum-tree

---
+++ problems/binary-search-tree-to-greater-sum-tree/src/main.rs
@@ -0,0 +1,126 @@
+fn main() {
+    assert_eq!(
+        Solution::bst_to_gst(Some(Rc::new(RefCell::new(TreeNode {
+            val: 4,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 0,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 3,
+                        left: None,
+                        right: None,
+                    }))),
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 6,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 7,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 8,
+                        left: None,
+                        right: None,
+                    }))),
+                }))),
+            }))),
+        })))),
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 30,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 36,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 36,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 35,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 33,
+                        left: None,
+                        right: None,
+                    }))),
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 21,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 26,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 15,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 8,
+                        left: None,
+                        right: None,
+                    }))),
+                }))),
+            }))),
+        })))
+    );
+
+    assert_eq!(
+        Solution::bst_to_gst(Some(Rc::new(RefCell::new(TreeNode {
+            val: 0,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: None,
+            }))),
+        })))),
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: None,
+            }))),
+        })))
+    );
+}
+
+struct Solution;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn bst_to_gst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        return None;
+    }
+}
