3bb6056a1e34b74a2778fb575f02895fe04d8f35
akaishi <galigalikun@gmail.com>
2023-06-03 21:29:11 +0900

    wip moving-stones-until-consecutive-ii

---
+++ problems/moving-stones-until-consecutive-ii/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::num_moves_stones_ii(vec![7,4,9]), vec![1,2]);
+    assert_eq!(Solution::num_moves_stones_ii(vec![6,5,4,3,10]), vec![2,3]);
+}
+
+struct Solution;
+impl Solution {
+    pub fn num_moves_stones_ii(stones: Vec<i32>) -> Vec<i32> {
+        return vec![];
+    }
+}
