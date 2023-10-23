f28106110aa63fd8e22a3d79cc017bd1a29da8b4
akaishi <galigalikun@gmail.com>
2023-10-23 21:49:08 +0900

    add average-selling-price

---
+++ sql/average-selling-price.sql
@@ -0,0 +1,10 @@
+-- # Write your MySQL query statement below
+select 
+Prices.product_id,
+ifnull(round(sum(Prices.price*UnitsSold.units)/sum(UnitsSold.units), 2),0) as average_price
+from Prices
+left join UnitsSold
+on Prices.product_id=UnitsSold.product_id
+and Prices.start_date <= UnitsSold.purchase_date
+and Prices.end_date >= UnitsSold.purchase_date
+group by Prices.product_id
