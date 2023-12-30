8c00d2a4ab13829749c231929679de0c9633b973
akaishi <galigalikun@gmail.com>
2023-12-30 09:34:33 +0900

    wip delete-leaves-with-a-given-value

---
+++ problems/delete-leaves-with-a-given-value/src/main.rs
@@ -0,0 +1,154 @@
+fn main() {
+    assert_eq!(
+        Solution::remove_leaf_nodes(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: None,
+                        right: None
+                    }))),
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: None,
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    })))
+                })))
+            }))),
+            2
+        ),
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None
+                })))
+            })))
+        })))
+    );
+
+    assert_eq!(
+        Solution::remove_leaf_nodes(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 3,
+                        left: None,
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            3
+        ),
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            right: None
+        })))
+    );
+
+    assert_eq!(
+        Solution::remove_leaf_nodes(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 2,
+                        left: Some(Rc::new(RefCell::new(TreeNode {
+                            val: 3,
+                            left: None,
+                            right: None
+                        }))),
+                        right: None
+                    }))),
+                    right: None
+                }))),
+                right: None
+            }))),
+            2
+        ),
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: None
+        })))
+    );
+}
+
+struct Solution;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn remove_leaf_nodes(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        target: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = root {
+            {
+                let mut node = node.borrow_mut();
+                node.left = Self::remove_leaf_nodes(node.left.take(), target);
+                node.right = Self::remove_leaf_nodes(node.right.take(), target);
+                if node.left.is_none() && node.right.is_none() && node.val == target {
+                    return None;
+                }
+            }
+            return Some(node);
+        }
+        return None;
+    }
+}
