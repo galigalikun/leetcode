0a766783d00ff93813f53e68d8f58fb64a9dc7a2
akaishi <galigalikun@gmail.com>
2024-08-04 00:05:00 +0900

    feat: Add code for number-of-good-leaf-nodes-pairs problem

---
+++ problems/number-of-good-leaf-nodes-pairs/src/main.rs
@@ -0,0 +1,123 @@
+fn main() {
+    assert_eq!(
+        Solution::count_pairs(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None
+                })))
+            }))),
+            3
+        ),
+        1
+    );
+    assert_eq!(
+        Solution::count_pairs(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: None,
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 5,
+                        left: None,
+                        right: None
+                    })))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 6,
+                        left: None,
+                        right: None
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 7,
+                        left: None,
+                        right: None
+                    })))
+                })))
+            }))),
+            3
+        ),
+        2
+    );
+
+    assert_eq!(
+        Solution::count_pairs(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 7,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 6,
+                        left: Some(Rc::new(RefCell::new(TreeNode {
+                            val: 3,
+                            left: None,
+                            right: Some(Rc::new(RefCell::new(TreeNode {
+                                val: 2,
+                                left: None,
+                                right: None
+                            })))
+                        }))),
+                        right: None
+                    }))),
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 5,
+                        left: None,
+                        right: None
+                    })))
+                })))
+            }))),
+            3
+        ),
+        1
+    );
+}
+
+struct Solution;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn count_pairs(root: Option<Rc<RefCell<TreeNode>>>, distance: i32) -> i32 {
+        return 0;
+    }
+}
