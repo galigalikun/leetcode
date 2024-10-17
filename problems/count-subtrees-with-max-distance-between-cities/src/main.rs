f39c500e8b365435351abd46b2d9d5d13f1cb21a
akaishi <galigalikun@gmail.com>
2024-10-17 22:58:12 +0900

    Add code for implementing the "count-subtrees-with-max-distance-between-cities" problem

---
+++ problems/count-subtrees-with-max-distance-between-cities/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    assert_eq!(
+        Solution::count_subgraphs_for_each_diameter(4, vec![vec![1, 2], vec![2, 3], vec![2, 4]]),
+        vec![3, 4, 0]
+    );
+    assert_eq!(
+        Solution::count_subgraphs_for_each_diameter(2, vec![vec![1, 2]]),
+        vec![1]
+    );
+    assert_eq!(
+        Solution::count_subgraphs_for_each_diameter(3, vec![vec![1, 2], vec![2, 3]]),
+        vec![2, 1]
+    );
+}
+
+struct Solution;
+impl Solution {
+    pub fn count_subgraphs_for_each_diameter(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut graph = vec![vec![0; n as usize]; n as usize];
+        for edge in edges.iter() {
+            let (a, b) = (edge[0] as usize - 1, edge[1] as usize - 1);
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+        }
+        let mut res = vec![0; n as usize - 1];
+        for i in 1..(1 << n) {
+            let mut nodes = vec![];
+            for j in 0..n {
+                if i & (1 << j) != 0 {
+                    nodes.push(j as usize);
+                }
+            }
+            let mut g = vec![vec![0; n as usize]; n as usize];
+            for &node in nodes.iter() {
+                for &node2 in nodes.iter() {
+                    g[node][node2] = graph[node][node2];
+                }
+            }
+            let mut d = vec![vec![n; n as usize]; n as usize];
+            for &node in nodes.iter() {
+                d[node][node] = 0;
+            }
+            for &node in nodes.iter() {
+                for &node2 in nodes.iter() {
+                    if g[node][node2] == 1 {
+                        d[node][node2] = 1;
+                    }
+                }
+            }
+        }
+        res
+    }
+}
