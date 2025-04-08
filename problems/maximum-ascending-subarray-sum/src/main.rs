83beb7d49ebd9abf51177765ea3bd65e077f9943
akaishi <galigalikun@gmail.com>
2025-04-08 09:20:18 +0900

    Refactor max_ascending_sum function for improved clarity and performance

--- problems/maximum-ascending-subarray-sum/src/main.rs
+++ problems/maximum-ascending-subarray-sum/src/main.rs
@@ -10,10 +10,24 @@ fn main() {
 struct Solution;
 impl Solution {
     pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {
-        return nums
-            .split(|&x| x <= 0)
-            .map(|slice| slice.iter().fold(0, |acc, &x| acc + x))
-            .max()
-            .unwrap_or(0);
+        let mut result = vec![];
+        let mut sum = 0;
+        for i in 0..nums.len() {
+            if i == 0 || nums[i] > nums[i - 1] {
+                sum += nums[i];
+            } else {
+                result.push(sum);
+                sum = nums[i];
+            }
+        }
+        result.push(sum);
+        result.sort();
+        result.reverse();
+
+        if result.len() > 0 {
+            return result[0];
+        } else {
+            return 0;
+        }
     }
 }
