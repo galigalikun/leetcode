36f1555e747af166deb270d8b20052b3c3e63304
akaishi <galigalikun@gmail.com>
2025-08-03 02:57:51 +0900

    Add initial implementation for "merge-bsts-to-create-single-bst" problem

---
+++ problems/merge-bsts-to-create-single-bst/src/main.rs
@@ -0,0 +1,96 @@
+fn main() {
+    assert_eq!(
+        Solution::can_merge(vec![
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: None,
+                    right: None,
+                }))),
+                right: None,
+            }))),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                }))),
+                right: None,
+            }))),
+        ]),
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: None,
+                right: None,
+            }))),
+        })))
+    );
+}
+
+struct Solution;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn can_merge(trees: Vec<Option<Rc<RefCell<TreeNode>>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut map = std::collections::HashMap::new();
+        let mut roots = vec![];
+        for tree in trees {
+            if let Some(node) = tree {
+                let node_ref = node.borrow();
+                map.insert(node_ref.val, node.clone());
+                if node_ref.left.is_none() && node_ref.right.is_none() {
+                    roots.push(node.clone());
+                }
+            }
+        }
+        None
+    }
+}
