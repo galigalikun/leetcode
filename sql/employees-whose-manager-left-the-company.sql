270543dd9e9216abc5b9d7a91228ed0f60f66048
akaishi <galigalikun@gmail.com>
2025-09-14 01:59:59 +0900

    Add initial implementation for "employees-whose-manager-left-the-company" SQL query

---
+++ sql/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,8 @@
+-- # Write your MySQL query statement below
+select  e1.employee_id
+from Employees e1
+left join Employees e2
+on e1.manager_id = e2.employee_id
+where e2.employee_id is null
+and e1.manager_id is not null
+order by e1.employee_id;
