82ee7bfa72d2be7bae6ef3382e04665b4452ebce
akaishi <galigalikun@gmail.com>
2026-01-12 01:49:38 +0900

    Add implementation for set intersection size at least two problem

--- problems/set-intersection-size-at-least-two/src/main.rs
+++ problems/set-intersection-size-at-least-two/src/main.rs
@@ -12,9 +12,34 @@ fn main() {
 struct Solution {}
 impl Solution {
     pub fn intersection_size_two(intervals: Vec<Vec<i32>>) -> i32 {
-        for (i, v) in intervals.iter().enumerate() {
-            println!("{} {} {}", i, v[0], v[1]);
+        let mut intervals = intervals;
+        intervals.sort_by(|a, b| {
+            if a[1] == b[1] {
+                b[0].cmp(&a[0])
+            } else {
+                a[1].cmp(&b[1])
+            }
+        });
+
+        let mut answer = 0;
+        let mut first = -1; // second-largest chosen value
+        let mut second = -1; // largest chosen value
+
+        for interval in intervals {
+            let start = interval[0];
+            let end = interval[1];
+
+            if start > second {
+                answer += 2;
+                first = end - 1;
+                second = end;
+            } else if start > first {
+                answer += 1;
+                first = second;
+                second = end;
+            }
         }
-        return -1;
+
+        answer
     }
 }
