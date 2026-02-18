5b89daa74ef4769640aa1e7bb677611cd5e3d551
akaishi <galigalikun@gmail.com>
2026-02-18 21:33:05 +0900

    Implement largest perimeter triangle calculation logic

--- problems/largest-perimeter-triangle/src/main.rs
+++ problems/largest-perimeter-triangle/src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
-    assert_eq!(Solution::largest_perimeter(vec![2,1,2]), 5);
-    assert_eq!(Solution::largest_perimeter(vec![1,2,1,10]), 10);
+    assert_eq!(Solution::largest_perimeter(vec![2, 1, 2]), 5);
+    assert_eq!(Solution::largest_perimeter(vec![1, 2, 1, 10]), 0);
 }
 
 struct Solution;
 impl Solution {
-    pub fn largest_perimeter(nums: Vec<i32>) -> i32 {
-        return 0;
+    pub fn largest_perimeter(mut nums: Vec<i32>) -> i32 {
+        nums.sort_unstable_by(|a, b| b.cmp(a));
+        for i in 0..nums.len() - 2 {
+            if nums[i] < nums[i + 1] + nums[i + 2] {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        0
     }
 }
