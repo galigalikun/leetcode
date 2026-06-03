55f71e24afbd3e2ad0e7f7add950bb83b5df058a
akaishi <galigalikun@gmail.com>
2026-06-03 09:59:19 +0900

    Refactor maximum_swap function to improve logic and enhance readability

--- problems/maximum-swap/src/main.rs
+++ problems/maximum-swap/src/main.rs
@@ -3,13 +3,30 @@ fn main() {
     assert_eq!(Solution::maximum_swap(9973), 9973);
 }
 
-struct Solution{}
+struct Solution {}
 impl Solution {
     pub fn maximum_swap(num: i32) -> i32 {
-        let mut n = num;
-        for i in 1..num.to_string().len() {
-            println!("debug {:?}", num.to_string().chars().nth(i));
+        let mut num = num;
+        let mut digits = Vec::new();
+        while num > 0 {
+            digits.push(num % 10);
+            num /= 10;
         }
-        return 0;
+        digits.reverse();
+
+        let mut last = [0; 10];
+        for (i, &d) in digits.iter().enumerate() {
+            last[d as usize] = i;
+        }
+
+        for (i, &d) in digits.iter().enumerate() {
+            for j in (d + 1..=9).rev() {
+                if last[j as usize] > i {
+                    digits.swap(i, last[j as usize]);
+                    return digits.into_iter().fold(0, |acc, d| acc * 10 + d);
+                }
+            }
+        }
+        digits.into_iter().fold(0, |acc, d| acc * 10 + d)
     }
 }
