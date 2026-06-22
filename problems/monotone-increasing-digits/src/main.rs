7ab350de77008b96bc853fee9cc8b77d0b257559
akaishi <galigalikun@gmail.com>
2026-06-22 21:35:24 +0900

    Implement monotone_increasing_digits function to calculate the largest monotone increasing number less than or equal to n

--- problems/monotone-increasing-digits/src/main.rs
+++ problems/monotone-increasing-digits/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    assert_eq!(Solution::monotone_increasing_digits(1), 9);
+    assert_eq!(Solution::monotone_increasing_digits(1), 1);
+    assert_eq!(Solution::monotone_increasing_digits(10), 9);
     assert_eq!(Solution::monotone_increasing_digits(1234), 1234);
     assert_eq!(Solution::monotone_increasing_digits(332), 299);
 }
@@ -7,6 +8,22 @@ fn main() {
 struct Solution{}
 impl Solution {
     pub fn monotone_increasing_digits(n: i32) -> i32 {
-        return 0;
+        let mut digits: Vec<u8> = n.to_string().bytes().collect();
+        let mut marker = digits.len();
+
+        for index in (1..digits.len()).rev() {
+            if digits[index - 1] > digits[index] {
+                digits[index - 1] -= 1;
+                marker = index;
+            }
+        }
+
+        for digit in digits.iter_mut().skip(marker) {
+            *digit = b'9';
+        }
+
+        digits
+            .into_iter()
+            .fold(0, |accumulator, digit| accumulator * 10 + (digit - b'0') as i32)
     }
 }
