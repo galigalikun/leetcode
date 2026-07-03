acb608afeec3aa79f872aca8fc07a70a42454058
akaishi <galigalikun@gmail.com>
2026-07-04 08:03:02 +0900

    Implement max_chunks_to_sorted function to calculate the maximum number of chunks that can be sorted

--- problems/max-chunks-to-make-sorted-ii/src/main.rs
+++ problems/max-chunks-to-make-sorted-ii/src/main.rs
@@ -6,7 +6,30 @@ fn main() {
 struct Solution{}
 impl Solution {
     pub fn max_chunks_to_sorted(arr: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
 
-        return 0;
+        let mut sorted = arr.clone();
+        sorted.sort_unstable();
+
+        let mut delta: HashMap<i32, i32> = HashMap::new();
+        let mut chunks = 0;
+
+        for (a, b) in arr.into_iter().zip(sorted.into_iter()) {
+            *delta.entry(a).or_insert(0) += 1;
+            if delta.get(&a) == Some(&0) {
+                delta.remove(&a);
+            }
+
+            *delta.entry(b).or_insert(0) -= 1;
+            if delta.get(&b) == Some(&0) {
+                delta.remove(&b);
+            }
+
+            if delta.is_empty() {
+                chunks += 1;
+            }
+        }
+
+        chunks
     }
 }
