e5444449567b0c9b8298877292452683d3e76b71
akaishi <galigalikun@gmail.com>
2026-07-11 21:37:44 +0900

    Refactor moves_to_chessboard function to improve logic and readability

--- problems/transform-to-chessboard/src/main.rs
+++ problems/transform-to-chessboard/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
         Solution::moves_to_chessboard(vec![
             vec![0, 1, 1, 0],
             vec![0, 1, 1, 0],
-            [1, 0, 0, 1],
+            vec![1, 0, 0, 1],
             vec![1, 0, 0, 1]
         ]),
         2
@@ -21,6 +21,50 @@ fn main() {
 struct Solution {}
 impl Solution {
     pub fn moves_to_chessboard(board: Vec<Vec<i32>>) -> i32 {
-        return -1;
+        let n = board.len();
+
+        for i in 0..n {
+            for j in 0..n {
+                if (board[0][0] ^ board[i][0] ^ board[0][j] ^ board[i][j]) != 0 {
+                    return -1;
+                }
+            }
+        }
+
+        let row_sum: usize = (0..n).map(|i| board[0][i] as usize).sum();
+        let col_sum: usize = (0..n).map(|i| board[i][0] as usize).sum();
+
+        if row_sum < n / 2 || row_sum > (n + 1) / 2 {
+            return -1;
+        }
+        if col_sum < n / 2 || col_sum > (n + 1) / 2 {
+            return -1;
+        }
+
+        let mut row_swaps = 0usize;
+        let mut col_swaps = 0usize;
+
+        for i in 0..n {
+            if board[i][0] == (i % 2) as i32 {
+                row_swaps += 1;
+            }
+            if board[0][i] == (i % 2) as i32 {
+                col_swaps += 1;
+            }
+        }
+
+        if n % 2 == 1 {
+            if row_swaps % 2 == 1 {
+                row_swaps = n - row_swaps;
+            }
+            if col_swaps % 2 == 1 {
+                col_swaps = n - col_swaps;
+            }
+        } else {
+            row_swaps = row_swaps.min(n - row_swaps);
+            col_swaps = col_swaps.min(n - col_swaps);
+        }
+
+        ((row_swaps + col_swaps) / 2) as i32
     }
 }
