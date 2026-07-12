1501866acd80319290f6e54c380d9c1e1b953eb0
akaishi <galigalikun@gmail.com>
2026-07-12 10:40:37 +0900

    Refactor letter_case_permutation function to improve logic and remove unnecessary code

--- problems/letter-case-permutation/src/main.rs
+++ problems/letter-case-permutation/src/main.rs
@@ -1,5 +1,3 @@
-use std::ascii::AsciiExt;
-
 fn main() {
     assert_eq!(Solution::letter_case_permutation("a1b2".to_string()), vec!["a1b2","a1B2","A1b2","A1B2"]);
     assert_eq!(Solution::letter_case_permutation("3z4".to_string()), vec!["3z4","3Z4"]);
@@ -8,12 +6,34 @@ fn main() {
 struct Solution{}
 impl Solution {
     pub fn letter_case_permutation(s: String) -> Vec<String> {
-        let mut ans = vec![1];
-        for (i, c) in s.chars().enumerate() {
+        let mut ans = vec![String::new()];
+
+        for c in s.chars() {
             if c.is_alphabetic() {
-                println!("debug {}", c.to_ascii_uppercase());
+                ans = ans
+                    .iter()
+                    .flat_map(|prefix| {
+                        let mut lower = prefix.clone();
+                        lower.push(c.to_ascii_lowercase());
+
+                        let mut upper = prefix.clone();
+                        upper.push(c.to_ascii_uppercase());
+
+                        [lower, upper]
+                    })
+                    .collect();
+            } else {
+                ans = ans
+                    .iter()
+                    .map(|prefix| {
+                        let mut next = prefix.clone();
+                        next.push(c);
+                        next
+                    })
+                    .collect();
             }
         }
-        return vec![];
+
+        ans
     }
 }
