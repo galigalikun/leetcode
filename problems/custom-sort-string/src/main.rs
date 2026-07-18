6309adef6bec10b6848bc908e5b18c3bba7f7eb1
akaishi <galigalikun@gmail.com>
2026-07-18 21:22:01 +0900

    Implement custom_sort_string function to sort characters based on a given order

--- problems/custom-sort-string/src/main.rs
+++ problems/custom-sort-string/src/main.rs
@@ -3,9 +3,36 @@ fn main() {
     assert_eq!(Solution::custom_sort_string("cbafg".to_string(), "abcd".to_string()), "cbad");
 }
 
-struct Solution{}
+struct Solution {}
+
 impl Solution {
     pub fn custom_sort_string(order: String, s: String) -> String {
-        return "".to_string();
+        let mut counts = [0_usize; 26];
+
+        for &b in s.as_bytes() {
+            let idx = (b - b'a') as usize;
+            counts[idx] += 1;
+        }
+
+        let mut result = String::with_capacity(s.len());
+
+        for &b in order.as_bytes() {
+            let idx = (b - b'a') as usize;
+            let count = counts[idx];
+
+            for _ in 0..count {
+                result.push(b as char);
+            }
+
+            counts[idx] = 0;
+        }
+
+        for (idx, &count) in counts.iter().enumerate() {
+            for _ in 0..count {
+                result.push((b'a' + idx as u8) as char);
+            }
+        }
+
+        result
     }
 }
