f0a5a137b727b410326d9afccf5b3543641026aa
akaishi <galigalikun@gmail.com>
2026-07-24 21:49:37 +0900

    Implement eventual_safe_nodes function to identify all eventual safe nodes in a directed graph

--- problems/find-eventual-safe-states/src/main.rs
+++ problems/find-eventual-safe-states/src/main.rs
@@ -6,6 +6,33 @@ fn main() {
 struct Solution{}
 impl Solution {
     pub fn eventual_safe_nodes(graph: Vec<Vec<i32>>) -> Vec<i32> {
-        return vec![];
+        fn is_safe(node: usize, graph: &[Vec<i32>], state: &mut [u8]) -> bool {
+            if state[node] != 0 {
+                return state[node] == 2;
+            }
+
+            state[node] = 1;
+            for &next in &graph[node] {
+                let next_node = next as usize;
+                if state[next_node] == 1 || !is_safe(next_node, graph, state) {
+                    return false;
+                }
+            }
+
+            state[node] = 2;
+            true
+        }
+
+        let n = graph.len();
+        let mut state = vec![0_u8; n];
+        let mut answer = Vec::new();
+
+        for node in 0..n {
+            if is_safe(node, &graph, &mut state) {
+                answer.push(node as i32);
+            }
+        }
+
+        answer
     }
 }
