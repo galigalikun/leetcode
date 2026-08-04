d6f13290fcc71691f0a82782394bdd049cec2e5b
akaishi <galigalikun@gmail.com>
2026-08-05 00:19:09 +0900

    Refactor num_friend_requests function to improve efficiency using counting and prefix sums

--- problems/friends-of-appropriate-ages/src/main.rs
+++ problems/friends-of-appropriate-ages/src/main.rs
@@ -1,24 +1,34 @@
 fn main() {
     assert_eq!(Solution::num_friend_requests(vec![16, 16]), 2);
     assert_eq!(Solution::num_friend_requests(vec![16, 17, 18]), 2);
+    assert_eq!(Solution::num_friend_requests(vec![20, 30, 100, 110, 120]), 3);
 }
 
 struct Solution {}
 impl Solution {
     pub fn num_friend_requests(ages: Vec<i32>) -> i32 {
-        let mut ans = 0;
-        for (i, &age1) in ages.iter().enumerate() {
-            for (j, &age2) in ages.iter().enumerate() {
-                if i != j {
-                    if age1 as f64 > 0.5 * age2 as f64 + 7f64
-                        && age1 < age2
-                        && (age1 <= 100 || age2 > 100)
-                    {
-                        ans += 1;
-                    }
-                }
-            }
+        let mut count = [0_i32; 121];
+        for age in ages {
+            count[age as usize] += 1;
         }
-        return ans;
+
+        let mut prefix = [0_i32; 121];
+        for age in 1..=120 {
+            prefix[age] = prefix[age - 1] + count[age];
+        }
+
+        let mut total = 0_i32;
+        for age_x in 15..=120 {
+            let c_x = count[age_x];
+            if c_x == 0 {
+                continue;
+            }
+
+            let min_age_y = age_x / 2 + 7;
+            let eligible_y = prefix[age_x] - prefix[min_age_y];
+            total += c_x * (eligible_y - 1);
+        }
+
+        total
     }
 }
