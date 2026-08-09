8c57313699b9ca4e27825a06dc32d2d48216095b
akaishi <galigalikun@gmail.com>
2026-08-09 11:50:42 +0900

    Refactor is_rectangle_overlap function to simplify overlap logic and improve readability

--- problems/rectangle-overlap/src/main.rs
+++ problems/rectangle-overlap/src/main.rs
@@ -28,11 +28,9 @@ fn main() {
 struct Solution {}
 impl Solution {
     pub fn is_rectangle_overlap(rec1: Vec<i32>, rec2: Vec<i32>) -> bool {
-        if rec1[0] < rec2[0] && rec1[1] <= rec2[1] && rec1[2] > rec2[0] && rec1[3] > rec2[1] {
-            return true;
-        } else if rec1 == rec2 {
-            return true;
-        }
-        return false;
+        let overlap_x = rec1[0].max(rec2[0]) < rec1[2].min(rec2[2]);
+        let overlap_y = rec1[1].max(rec2[1]) < rec1[3].min(rec2[3]);
+
+        overlap_x && overlap_y
     }
 }
