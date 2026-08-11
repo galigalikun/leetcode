2d5197a263eb7618b859e115fbecfd2a3d006179
akaishi <galigalikun@gmail.com>
2026-08-11 22:43:04 +0900

    Implement split_into_fibonacci function to generate Fibonacci sequences from a string input

--- problems/split-array-into-fibonacci-sequence/src/main.rs
+++ problems/split-array-into-fibonacci-sequence/src/main.rs
@@ -13,6 +13,51 @@ fn main() {
 struct Solution {}
 impl Solution {
     pub fn split_into_fibonacci(num: String) -> Vec<i32> {
-        return vec![];
+        let bytes = num.as_bytes();
+        let mut sequence = Vec::new();
+
+        fn dfs(index: usize, bytes: &[u8], sequence: &mut Vec<i32>) -> bool {
+            if index == bytes.len() {
+                return sequence.len() >= 3;
+            }
+
+            let mut value: i64 = 0;
+            for i in index..bytes.len() {
+                if i > index && bytes[index] == b'0' {
+                    break;
+                }
+
+                value = value * 10 + (bytes[i] - b'0') as i64;
+                if value > i32::MAX as i64 {
+                    break;
+                }
+
+                let current = value as i32;
+                let len = sequence.len();
+                if len >= 2 {
+                    let sum = sequence[len - 1] as i64 + sequence[len - 2] as i64;
+                    if value < sum {
+                        continue;
+                    }
+                    if value > sum {
+                        break;
+                    }
+                }
+
+                sequence.push(current);
+                if dfs(i + 1, bytes, sequence) {
+                    return true;
+                }
+                sequence.pop();
+            }
+
+            false
+        }
+
+        if dfs(0, bytes, &mut sequence) {
+            sequence
+        } else {
+            vec![]
+        }
     }
 }
