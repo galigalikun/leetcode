e5bd8175ce3b0ca2a25b81cc61a3d1d5ab3dd969
akaishi <galigalikun@gmail.com>
2026-08-17 21:37:55 +0900

    Implement score_of_parentheses function to calculate the score of a valid parentheses string

--- problems/score-of-parentheses/src/main.rs
+++ problems/score-of-parentheses/src/main.rs
@@ -7,6 +7,21 @@ fn main() {
 struct Solution;
 impl Solution {
     pub fn score_of_parentheses(s: String) -> i32 {
-        return 0;
+        let bytes = s.as_bytes();
+        let mut depth: usize = 0;
+        let mut score = 0;
+
+        for i in 0..bytes.len() {
+            if bytes[i] == b'(' {
+                depth += 1;
+            } else {
+                depth -= 1;
+                if i > 0 && bytes[i - 1] == b'(' {
+                    score += 1 << depth;
+                }
+            }
+        }
+
+        score
     }
 }
