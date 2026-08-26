d4f773322f818769ae52f4c5c855b74787994718
akaishi <galigalikun@gmail.com>
2026-08-26 22:05:26 +0900

    Implement possible_bipartition function to determine if a graph can be bipartitioned based on dislikes

--- problems/possible-bipartition/src/main.rs
+++ problems/possible-bipartition/src/main.rs
@@ -19,6 +19,39 @@ fn main() {
 struct Solution;
 impl Solution {
     pub fn possible_bipartition(n: i32, dislikes: Vec<Vec<i32>>) -> bool {
-        return false;
+        let size = n as usize;
+        let mut graph = vec![Vec::new(); size + 1];
+
+        for edge in dislikes {
+            let a = edge[0] as usize;
+            let b = edge[1] as usize;
+            graph[a].push(b);
+            graph[b].push(a);
+        }
+
+        let mut color = vec![0_i8; size + 1];
+        let mut queue = std::collections::VecDeque::new();
+
+        for person in 1..=size {
+            if color[person] != 0 {
+                continue;
+            }
+
+            color[person] = 1;
+            queue.push_back(person);
+
+            while let Some(current) = queue.pop_front() {
+                for &neighbor in &graph[current] {
+                    if color[neighbor] == 0 {
+                        color[neighbor] = -color[current];
+                        queue.push_back(neighbor);
+                    } else if color[neighbor] == color[current] {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        true
     }
 }
