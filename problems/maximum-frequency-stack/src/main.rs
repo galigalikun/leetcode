add3006466eab10cd4cc25a593aea83b8855a6c7
akaishi <galigalikun@gmail.com>
2026-09-03 08:17:49 +0900

    Implement FreqStack to manage elements by frequency with push and pop operations

--- problems/maximum-frequency-stack/src/main.rs
+++ problems/maximum-frequency-stack/src/main.rs
@@ -1,31 +1,54 @@
+use std::collections::HashMap;
+
 struct FreqStack {
-    stack: Vec<i32>,
+    freq: HashMap<i32, i32>,
+    groups: HashMap<i32, Vec<i32>>,
+    max_freq: i32,
 }
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl FreqStack {
     fn new() -> Self {
-        FreqStack { stack: vec![] }
+        Self {
+            freq: HashMap::new(),
+            groups: HashMap::new(),
+            max_freq: 0,
+        }
     }
 
     fn push(&mut self, val: i32) {
-        self.stack.push(val);
+        let next_freq = self.freq.get(&val).copied().unwrap_or(0) + 1;
+        self.freq.insert(val, next_freq);
+
+        self.groups.entry(next_freq).or_default().push(val);
+        self.max_freq = self.max_freq.max(next_freq);
     }
 
     fn pop(&mut self) -> i32 {
-        return self.stack.pop().unwrap_or(0);
+        let group = self
+            .groups
+            .get_mut(&self.max_freq)
+            .expect("pop is called only when stack is not empty");
+
+        let val = group
+            .pop()
+            .expect("max frequency group is non-empty when popping");
+
+        if group.is_empty() {
+            self.groups.remove(&self.max_freq);
+            self.max_freq -= 1;
+        }
+
+        if let Some(current_freq) = self.freq.get_mut(&val) {
+            *current_freq -= 1;
+            if *current_freq == 0 {
+                self.freq.remove(&val);
+            }
+        }
+
+        val
     }
 }
 
-/**
- * Your FreqStack object will be instantiated and called as such:
- * let obj = FreqStack::new();
- * obj.push(val);
- * let ret_2: i32 = obj.pop();
- */
 fn main() {
     let mut obj = FreqStack::new();
     obj.push(5);
@@ -34,6 +57,7 @@ fn main() {
     obj.push(7);
     obj.push(4);
     obj.push(5);
+
     assert_eq!(5, obj.pop());
     assert_eq!(7, obj.pop());
     assert_eq!(5, obj.pop());
