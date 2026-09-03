7b1593db4e8978ea58b55f52804f5c5bb70bc5ae
akaishi <galigalikun@gmail.com>
2026-09-03 20:14:14 +0900

    Implement subarray_bitwise_o_rs function to calculate unique bitwise ORs of subarrays

--- problems/bitwise-ors-of-subarrays/src/main.rs
+++ problems/bitwise-ors-of-subarrays/src/main.rs
@@ -1,11 +1,33 @@
 fn main() {
     assert_eq!(Solution::subarray_bitwise_o_rs(vec![0]), 1);
     assert_eq!(Solution::subarray_bitwise_o_rs(vec![1,1,2]), 3);
+    assert_eq!(Solution::subarray_bitwise_o_rs(vec![1, 2, 4]), 6);
+    assert_eq!(Solution::subarray_bitwise_o_rs(vec![3]), 1);
 }
 
 struct Solution;
 impl Solution {
     pub fn subarray_bitwise_o_rs(arr: Vec<i32>) -> i32 {
-        return 0;
+        use std::collections::HashSet;
+
+        let mut all_values: HashSet<i32> = HashSet::new();
+        let mut prev_end_values: HashSet<i32> = HashSet::new();
+
+        for value in arr {
+            let mut curr_end_values: HashSet<i32> = HashSet::new();
+            curr_end_values.insert(value);
+
+            for prev in &prev_end_values {
+                curr_end_values.insert(prev | value);
+            }
+
+            for current in &curr_end_values {
+                all_values.insert(*current);
+            }
+
+            prev_end_values = curr_end_values;
+        }
+
+        all_values.len() as i32
     }
 }
