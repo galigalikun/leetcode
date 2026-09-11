e6ef0817018b9cc1a1918f24474ebc25b8f61179
akaishi <galigalikun@gmail.com>
2026-09-12 06:23:06 +0900

    Implement TopVotedCandidate struct and methods for tracking votes and querying leaders

--- problems/online-election/src/main.rs
+++ problems/online-election/src/main.rs
@@ -1,20 +1,42 @@
+use std::collections::HashMap;
+
 struct TopVotedCandidate {
-
+    times: Vec<i32>,
+    leaders: Vec<i32>,
 }
-
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl TopVotedCandidate {
-
     fn new(persons: Vec<i32>, times: Vec<i32>) -> Self {
-        TopVotedCandidate {  }
+        let mut vote_count: HashMap<i32, i32> = HashMap::new();
+        let mut leaders: Vec<i32> = Vec::with_capacity(persons.len());
+        let mut current_leader = -1;
+        let mut current_max_votes = 0;
+
+        for person in persons {
+            let count = vote_count.entry(person).or_insert(0);
+            *count += 1;
+
+            if *count >= current_max_votes {
+                current_max_votes = *count;
+                current_leader = person;
+            }
+
+            leaders.push(current_leader);
+        }
+
+        TopVotedCandidate { times, leaders }
     }
 
     fn q(&self, t: i32) -> i32 {
-        return 0;
+        match self.times.binary_search(&t) {
+            Ok(index) => self.leaders[index],
+            Err(index) if index > 0 => self.leaders[index - 1],
+            Err(_) => self.leaders[0],
+        }
     }
 }
 
