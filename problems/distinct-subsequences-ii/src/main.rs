f84531f0bf6460a78df6a8c1f2c076a5400b47ca
akaishi <galigalikun@gmail.com>
2026-09-23 12:40:07 +0900

    Implement distinct_subseq_ii function to calculate the number of distinct subsequences of a string

--- problems/distinct-subsequences-ii/src/main.rs
+++ problems/distinct-subsequences-ii/src/main.rs
@@ -7,6 +7,23 @@ fn main() {
 struct Solution;
 impl Solution {
     pub fn distinct_subseq_ii(s: String) -> i32 {
-        return 0;
+        const MOD: i64 = 1_000_000_007;
+
+        let mut total: i64 = 0;
+        let mut end_with = [0_i64; 26];
+
+        for b in s.bytes() {
+            let index = (b - b'a') as usize;
+            let new_count = (total + 1) % MOD;
+
+            total = (total + new_count - end_with[index]) % MOD;
+            if total < 0 {
+                total += MOD;
+            }
+
+            end_with[index] = new_count;
+        }
+
+        total as i32
     }
 }
